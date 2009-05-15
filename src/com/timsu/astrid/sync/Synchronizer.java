0031ca9d82d88dea915f336f78e5d440a0a1eecd
Tim Su <tim@todoroo.com>
2009-05-15 21:23:14 +0000

    woe is me. Removed annotation, got the cast removed by save actions, and now it doesn't work.

--- src/com/timsu/astrid/sync/Synchronizer.java
+++ src/com/timsu/astrid/sync/Synchronizer.java
@@ -212,11 +212,11 @@ public class Synchronizer {
             typeClass = cls;
         }
 
-		public TYPE get(Context context) {
+		@SuppressWarnings("unchecked")
+        public TYPE get(Context context) {
             if(controller == null) {
                 try {
-                    controller = typeClass.getConstructors()[0].newInstance(
-                            context);
+                    controller = (TYPE)typeClass.getConstructors()[0].newInstance(context);
                 } catch (IllegalArgumentException e) {
                     Log.e(getClass().getSimpleName(), e.toString());
                 } catch (SecurityException e) {
