f8c638158ff5046b6f816ea06d20cc6aa262caac
Tim Su <tim@todoroo.com>
2010-04-24 00:48:39 -0700

    Cleaned up error messages when SD card doesn't exist.

--- src/com/timsu/astrid/utilities/TasksXmlExporter.java
+++ src/com/timsu/astrid/utilities/TasksXmlExporter.java
@@ -1,5 +1,16 @@
 package com.timsu.astrid.utilities;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.xmlpull.v1.XmlSerializer;
+
 import android.content.Context;
 import android.database.Cursor;
 import android.os.Environment;
@@ -7,6 +18,7 @@ import android.os.Looper;
 import android.util.Log;
 import android.util.Xml;
 import android.widget.Toast;
+
 import com.timsu.astrid.R;
 import com.timsu.astrid.data.alerts.AlertController;
 import com.timsu.astrid.data.sync.SyncDataController;
@@ -17,12 +29,6 @@ import com.timsu.astrid.data.tag.TagModelForView;
 import com.timsu.astrid.data.task.TaskController;
 import com.timsu.astrid.data.task.TaskIdentifier;
 import com.timsu.astrid.data.task.TaskModelForXml;
-import org.xmlpull.v1.XmlSerializer;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.*;
 
 public class TasksXmlExporter {
 
@@ -208,7 +214,7 @@ public class TasksXmlExporter {
                 return true;
             } else {
                 // Unable to make the /sdcard/astrid directory.
-                String error = ctx.getString(R.string.error_sdcard) + astridDir.getAbsolutePath();
+                String error = ctx.getString(R.string.error_sdcard, astridDir.getAbsolutePath());
                 Log.e("TasksXmlExporter", error);
                 if (!isService) {
                     displayErrorToast(error);
@@ -217,7 +223,7 @@ public class TasksXmlExporter {
             }
         } else {
             // Unable to access the sdcard because it's not in the mounted state.
-            String error = ctx.getString(R.string.error_sdcard);
+            String error = ctx.getString(R.string.error_sdcard_general);
             Log.e("TasksXmlExporter", error);
             if (!isService) {
                 displayErrorToast(error);
