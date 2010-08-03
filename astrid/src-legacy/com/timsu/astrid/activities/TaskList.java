934ff46fbbdb103d22c09c43c4143883874281ec
Tim Su <tim@todoroo.com>
2010-08-03 15:49:43 -0700

    bump 3.0.3 - created a legacy task list activity so users can use their old shortcut to launch Astrid

---
+++ astrid/src-legacy/com/timsu/astrid/activities/TaskList.java
@@ -0,0 +1,63 @@
+/*
+ * ASTRID: Android's Simple Task Recording Dashboard
+ *
+ * Copyright (c) 2009 Tim Su
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package com.timsu.astrid.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.todoroo.astrid.activity.TaskListActivity;
+
+/**
+ * Legacy task shortcut, takes users to the updated {@link TaskListActivity}.
+ * This activity is around so users with existing desktop icons will
+ * be able to still launch Astrid.
+ *
+ * @author Tim Su <tim@todoroo.com>
+ *
+ */
+public class TaskList extends Activity {
+
+    // --- implementation
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        launchTaskList(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        launchTaskList(intent);
+    }
+
+    /**
+     * intent: ignored for now
+     * @param intent
+     */
+    private void launchTaskList(Intent intent) {
+        Intent taskListIntent = new Intent(this, TaskListActivity.class);
+        startActivity(taskListIntent);
+        finish();
+    }
+}
