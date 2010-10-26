22ba0f49b94d32042d5f3819cb7570c3aa7ae4b8
Tim Su <tim@todoroo.com>
2010-10-25 18:31:11 -0700

    bump date

--- astrid/src/com/todoroo/astrid/utility/Constants.java
+++ astrid/src/com/todoroo/astrid/utility/Constants.java
@@ -35,7 +35,7 @@ public final class Constants {
     /**
      * Upgrade time
      */
-    public static final Date UPGRADE = new Date(110, 11, 1);
+    public static final Date UPGRADE = new Date(110, 10, 15);
 
     // --- notification id's
 
