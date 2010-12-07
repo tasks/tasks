4fa87a35d94ee875421877e55260c72618f5e2b7
Tim Su <tim@todoroo.com>
2010-12-06 18:59:29 -0800

    suppress strange motorola cliq error by subclassing edit text

---
+++ astrid/src/com/todoroo/astrid/ui/ErrorCatchingEditText.java
@@ -0,0 +1,43 @@
+package com.todoroo.astrid.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+public class ErrorCatchingEditText extends EditText {
+
+    public ErrorCatchingEditText(Context context, AttributeSet attrs,
+            int defStyle) {
+        super(context, attrs, defStyle);
+
+    }
+
+    public ErrorCatchingEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    public ErrorCatchingEditText(Context context) {
+        super(context);
+
+    }
+
+	@Override
+	public int getExtendedPaddingTop() {
+	    try {
+            return super.getExtendedPaddingTop();
+        } catch (Exception e) {
+            return 0;
+        }
+	}
+
+	@Override
+	public int getExtendedPaddingBottom() {
+	    try {
+            return super.getExtendedPaddingBottom();
+        } catch (Exception e) {
+            return 0;
+        }
+	}
+
+}
