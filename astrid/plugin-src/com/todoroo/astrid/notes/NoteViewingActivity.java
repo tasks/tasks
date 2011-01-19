abb456e0c7e161ea30fe5a359c56d401445a3dbb
Tim Su <tim@todoroo.com>
2011-01-18 17:56:04 -0800

    Added a dialog custom title style for note dialog

--- astrid/plugin-src/com/todoroo/astrid/notes/NoteViewingActivity.java
+++ astrid/plugin-src/com/todoroo/astrid/notes/NoteViewingActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.text.util.Linkify;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.timsu.astrid.R;
@@ -19,16 +21,25 @@ public class NoteViewingActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.empty_linear_layout);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.dialog_title_bar);
+
         LinearLayout body = (LinearLayout) findViewById(R.id.body);
 
         task = getIntent().getParcelableExtra(NotesActionExposer.EXTRA_TASK);
-        setTitle(task.getValue(Task.TITLE));
+        TextView title = (TextView) findViewById(R.id.title);
+        title.setText(task.getValue(Task.TITLE));
+
+        ScrollView scrollView = new ScrollView(this);
 
         TextView linkifiedTextView = new TextView(this);
         linkifiedTextView.setText(task.getValue(Task.NOTES) + "\n\n"); //$NON-NLS-1$
         Linkify.addLinks(linkifiedTextView, Linkify.ALL);
-        body.addView(linkifiedTextView);
+
+        scrollView.addView(linkifiedTextView);
+        body.addView(scrollView);
 
         Button ok = new Button(this);
         ok.setText(android.R.string.ok);
