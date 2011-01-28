de8125de748d7898e68f3cef93958a3417caed87
Joshua Gross <joshua.gross@gmail.com>
2011-01-28 00:07:13 -0600

    Send-to menu: only missing piece is filling in 'notes' field initially

---
+++ astrid/src/com/todoroo/astrid/activity/ShareLinkActivity.java
@@ -0,0 +1,33 @@
+/**
+ *
+ */
+package com.todoroo.astrid.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.todoroo.astrid.data.Task;
+
+/**
+ * @author joshuagross
+ *
+ * Create a new task based on incoming links from the "share" menu
+ */
+public final class ShareLinkActivity extends TaskListActivity {
+    public ShareLinkActivity () {
+        super();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent callerIntent = getIntent();
+
+        Task task = quickAddTask("", false);//$NON-NLS-1$
+        task.setValue(Task.NOTES, callerIntent.getStringExtra(Intent.EXTRA_TEXT));
+        Intent intent = new Intent(this, TaskEditActivity.class);
+        intent.putExtra(TaskEditActivity.TOKEN_ID, task.getId());
+        startActivityForResult(intent, ACTIVITY_EDIT_TASK);
+    }
+}
