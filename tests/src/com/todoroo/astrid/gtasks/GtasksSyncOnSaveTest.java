9e6c734e32ecef9a38d11b3f60a5c5832a1cb7ea
Sam Bosley <sboz88@gmail.com>
2011-08-10 19:38:20 -0700

    Fixed compile errors in gtasks tests

--- tests/src/com/todoroo/astrid/gtasks/GtasksSyncOnSaveTest.java
+++ tests/src/com/todoroo/astrid/gtasks/GtasksSyncOnSaveTest.java
@@ -147,7 +147,7 @@ public class GtasksSyncOnSaveTest extends DatabaseTestCase {
     }
 
     private boolean taskWithTitleExists(String title) throws IOException {
-        Tasks allTasks = gtasksService.getAllGtasksFromListId(DEFAULT_LIST, false);
+        Tasks allTasks = gtasksService.getAllGtasksFromListId(DEFAULT_LIST, false, false);
         if (allTasks.items != null) {
             for (com.google.api.services.tasks.v1.model.Task t : allTasks.items) {
                 if (t.title.equals(title))
@@ -209,7 +209,7 @@ public class GtasksSyncOnSaveTest extends DatabaseTestCase {
     }
 
     private void setupTestList() throws Exception {
-        Tasks defaultListTasks = gtasksService.getAllGtasksFromListId(DEFAULT_LIST, false);
+        Tasks defaultListTasks = gtasksService.getAllGtasksFromListId(DEFAULT_LIST, false, false);
         if (defaultListTasks.items != null) {
             for (com.google.api.services.tasks.v1.model.Task t : defaultListTasks.items) {
                 gtasksService.deleteGtask(DEFAULT_LIST, t.id);
