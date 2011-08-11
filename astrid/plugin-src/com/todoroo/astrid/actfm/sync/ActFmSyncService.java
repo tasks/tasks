8753e8d988e7943b92a6669b38c3fda4de60df7a
Tim Su <tim@todoroo.com>
2011-08-10 21:30:20 -0700

    If your task wasn't sent to Astrid.com, send the whole thing

--- astrid/plugin-src/com/todoroo/astrid/actfm/sync/ActFmSyncService.java
+++ astrid/plugin-src/com/todoroo/astrid/actfm/sync/ActFmSyncService.java
@@ -269,8 +269,10 @@ public final class ActFmSyncService {
 
         if(!newlyCreated) {
             params.add("id"); params.add(remoteId);
-        } else if(!values.containsKey(Task.TITLE.name))
+        } else if(!values.containsKey(Task.TITLE.name)) {
+            pushTask(task.getId());
             return;
+        }
 
         try {
             params.add("token"); params.add(token);
