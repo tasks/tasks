fccc8fa9635983182dde23c86f90ad95eb5cc98d
Sam Bosley <sboz88@gmail.com>
2011-11-04 19:38:21 -0700

    Fixed a dumb bug

--- astrid/plugin-src/com/todoroo/astrid/actfm/sync/ActFmInvoker.java
+++ astrid/plugin-src/com/todoroo/astrid/actfm/sync/ActFmInvoker.java
@@ -188,7 +188,7 @@ public class ActFmInvoker {
         String url = URL;
         if (method.startsWith("/"))
             url = url.replaceAll("/api/", "");
-        StringBuilder requestBuilder = new StringBuilder(URL).append(method).append('?');
+        StringBuilder requestBuilder = new StringBuilder(url).append(method).append('?');
         StringBuilder sigBuilder = new StringBuilder();
         for(Pair<String, Object> entry : params) {
             if(entry.getRight() == null)
