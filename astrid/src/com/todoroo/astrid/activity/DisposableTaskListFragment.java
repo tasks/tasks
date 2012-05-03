f382ddae10677de4773619152dc2defb638aa978
Sam Bosley <sboz88@gmail.com>
2012-05-02 23:44:29 -0700

    Fixed a bug in the DisposableTaskListFragment

--- astrid/src/com/todoroo/astrid/activity/DisposableTaskListFragment.java
+++ astrid/src/com/todoroo/astrid/activity/DisposableTaskListFragment.java
@@ -2,6 +2,9 @@ package com.todoroo.astrid.activity;
 
 import android.app.Activity;
 
+import com.timsu.astrid.R;
+import com.todoroo.andlib.utility.Preferences;
+
 /**
  * Task list fragment that will remove itself from the filterlist/fragment pager when it is detached
  * @author Sam
@@ -12,7 +15,8 @@ public class DisposableTaskListFragment extends TaskListFragment {
     @Override
     public void onDetach() {
         Activity activity = getActivity();
-        if (activity instanceof TaskListActivity) {
+        if (activity instanceof TaskListActivity &&
+                Preferences.getIntegerFromString(R.string.p_swipe_lists_performance_key, 0)> 0) {
             TaskListActivity tla = (TaskListActivity) activity;
             if (tla.getFragmentLayout() == AstridActivity.LAYOUT_SINGLE)
                 tla.getFragmentPagerAdapter().remove(filter);
