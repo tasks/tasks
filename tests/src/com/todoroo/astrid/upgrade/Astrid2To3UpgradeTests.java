e659651c18d5d24d7bf33212947230d5b41ce8ca
Tim Su <tim@todoroo.com>
2012-05-15 15:34:57 -0700

    Fix unit test compile error, fix coverage.em

--- tests/src/com/todoroo/astrid/upgrade/Astrid2To3UpgradeTests.java
+++ tests/src/com/todoroo/astrid/upgrade/Astrid2To3UpgradeTests.java
@@ -270,7 +270,7 @@ public class Astrid2To3UpgradeTests extends DatabaseTestCase {
         database.openForReading();
         TagService tagService = TagService.getInstance();
         Tag[] tags = tagService.getGroupedTags(TagService.GROUPED_TAGS_BY_ALPHA,
-                TaskCriteria.isActive());
+                TaskCriteria.isActive(), true);
         assertEquals(2, tags.length);
         assertEquals("salty", tags[0].tag);
         assertEquals("tasty", tags[1].tag);
@@ -280,13 +280,13 @@ public class Astrid2To3UpgradeTests extends DatabaseTestCase {
         assertEquals(3, tasks.getCount());
         tasks.moveToFirst();
         Task task = new Task(tasks);
-        assertEquals("tasty, salty", tagService.getTagsAsString(task.getId()));
+        assertEquals("tasty, salty", tagService.getTagsAsString(task.getId(), true));
         tasks.moveToNext();
         task.readFromCursor(tasks);
-        assertEquals("tasty", tagService.getTagsAsString(task.getId()));
+        assertEquals("tasty", tagService.getTagsAsString(task.getId(), true));
         tasks.moveToNext();
         task.readFromCursor(tasks);
-        assertEquals("", tagService.getTagsAsString(task.getId()));
+        assertEquals("", tagService.getTagsAsString(task.getId(), true));
     }
 
     /**
@@ -325,7 +325,7 @@ public class Astrid2To3UpgradeTests extends DatabaseTestCase {
         database.openForReading();
         TagService tagService = TagService.getInstance();
         Tag[] tags = tagService.getGroupedTags(TagService.GROUPED_TAGS_BY_ALPHA,
-                TaskCriteria.isActive());
+                TaskCriteria.isActive(), true);
         assertEquals(1, tags.length);
         assertEquals("attached", tags[0].tag);
 
@@ -334,10 +334,10 @@ public class Astrid2To3UpgradeTests extends DatabaseTestCase {
         assertEquals(2, tasks.getCount());
         tasks.moveToFirst();
         Task task = new Task(tasks);
-        assertEquals("attached", tagService.getTagsAsString(task.getId()));
+        assertEquals("attached", tagService.getTagsAsString(task.getId(), true));
         tasks.moveToNext();
         task.readFromCursor(tasks);
-        assertEquals("", tagService.getTagsAsString(task.getId()));
+        assertEquals("", tagService.getTagsAsString(task.getId(), true));
     }
 
     /**
