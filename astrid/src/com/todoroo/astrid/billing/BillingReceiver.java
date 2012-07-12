89767a10a4fed8bf8d942bbf867206d33c8af769
Sam Bosley <sboz88@gmail.com>
2012-07-12 13:25:08 -0700

    Started with billing classes

---
+++ astrid/src/com/todoroo/astrid/billing/BillingReceiver.java
@@ -0,0 +1,15 @@
+package com.todoroo.astrid.billing;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BillingReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
