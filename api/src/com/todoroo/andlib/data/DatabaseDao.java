1f8b094765fb1b24493780ab330f88018070785b
Tim Su <tim@astrid.com>
2012-07-24 18:37:41 -0700

    Replace 2009 Todoroo license

--- api/src/com/todoroo/andlib/data/DatabaseDao.java
+++ api/src/com/todoroo/andlib/data/DatabaseDao.java
@@ -1,7 +1,7 @@
-/*
- * Copyright (c) 2009, Todoroo Inc
- * All Rights Reserved
- * http://www.todoroo.com
+/**
+ * Copyright (c) 2012 Todoroo Inc
+ *
+ * See the file "LICENSE" for the full license governing this code.
  */
 package com.todoroo.andlib.data;
 
