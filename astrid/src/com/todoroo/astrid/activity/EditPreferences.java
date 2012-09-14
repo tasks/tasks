d9c3aea071cb17c5d8f4b6dabd85e6d8b2a95c7c
Sam Bosley <sboz88@gmail.com>
2012-09-13 17:59:26 -0700

    Fixed backwards settings descriptions

--- astrid/src/com/todoroo/astrid/activity/EditPreferences.java
+++ astrid/src/com/todoroo/astrid/activity/EditPreferences.java
@@ -546,13 +546,13 @@ public class EditPreferences extends TodorooPreferenceActivity {
         else if (booleanPreference(preference, value, R.string.p_autoIdea,
                 R.string.EPr_ideaAuto_desc_disabled, R.string.EPr_ideaAuto_desc_enabled));
         else if (booleanPreference(preference, value, R.string.p_field_missed_calls,
-                    R.string.MCA_missed_calls_pref_desc_enabled, R.string.MCA_missed_calls_pref_desc_disabled));
+                    R.string.MCA_missed_calls_pref_desc_disabled, R.string.MCA_missed_calls_pref_desc_enabled));
         else if (booleanPreference(preference, value, R.string.p_use_contact_picker,
-                    R.string.EPr_use_contact_picker_desc_enabled, R.string.EPr_use_contact_picker_desc_disabled));
+                    R.string.EPr_use_contact_picker_desc_disabled, R.string.EPr_use_contact_picker_desc_enabled));
         else if (booleanPreference(preference, value, R.string.p_third_party_addons,
                     R.string.EPr_third_party_addons_desc_enabled, R.string.EPr_third_party_addons_desc_disabled));
         else if (booleanPreference(preference, value, R.string.p_end_at_deadline,
-                    R.string.EPr_cal_start_at_due_time, R.string.EPr_cal_end_at_due_time));
+                    R.string.EPr_cal_end_at_due_time, R.string.EPr_cal_start_at_due_time));
         else if (r.getString(R.string.p_swipe_lists_enabled).equals(preference.getKey())) {
             preference.setOnPreferenceChangeListener(new SetResultOnPreferenceChangeListener(RESULT_CODE_PERFORMANCE_PREF_CHANGED) {
                 @Override
