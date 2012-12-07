0acfb48c548edf83d57e5407d12e72320a22f2cb
Sam Bosley <sboz88@gmail.com>
2012-12-06 17:28:08 -0800

    Fixed a compiler error

--- astrid/src/com/todoroo/astrid/service/abtesting/ABTests.java
+++ astrid/src/com/todoroo/astrid/service/abtesting/ABTests.java
@@ -41,10 +41,10 @@ public class ABTests {
             Account[] accounts = am.getAccounts();
             if (accounts == null || accounts.length == 0) {
                 addTest(AB_NEW_LOGIN_NO_GOOGLE, new int[] { 1, 1 },
-                        new int[] { 1, 0 }, new String[] { "old-welcome", "new-welcome" });  //$NON-NLS-1$//$NON-NLS-2$
+                        new int[] { 1, 0 }, new String[] { "old-welcome", "new-welcome" }, true);  //$NON-NLS-1$//$NON-NLS-2$
             } else {
                 addTest(AB_NEW_LOGIN_YES_GOOGLE, new int[] { 1, 1, 1 },
-                        new int[] { 1, 0, 0 }, new String[] { "old-welcome", "new-welcome", "new-quick-welcome" });  //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$
+                        new int[] { 1, 0, 0 }, new String[] { "old-welcome", "new-welcome", "new-quick-welcome" }, true);  //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$
             }
         }
     }
