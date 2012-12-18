1f0d01f8eaa86f1a8a44ae34af44a3ff4e203fba
Sam Bosley <sboz88@gmail.com>
2012-12-18 11:22:27 -0800

    Copy update, fixed a text color bug

--- astrid/src/com/todoroo/astrid/utility/AstridLitePreferenceSpec.java
+++ astrid/src/com/todoroo/astrid/utility/AstridLitePreferenceSpec.java
@@ -116,7 +116,7 @@ public class AstridLitePreferenceSpec extends AstridPreferenceSpec {
         setPreference(prefs, editor, r, R.string.p_show_menu_friends, false, ifUnset);
         setPreference(prefs, editor, r, R.string.p_show_featured_lists, false, ifUnset);
         setPreference(prefs, editor, r, R.string.p_show_menu_sync, false, ifUnset);
-        setPreference(prefs, editor, r, R.string.p_show_menu_sort, false, ifUnset);
+        setPreference(prefs, editor, r, R.string.p_show_menu_sort, true, ifUnset);
         setPreference(prefs, editor, r, R.string.p_show_menu_addons, false, ifUnset);
 
         setPreference(prefs, editor, r, R.string.p_show_quickadd_controls, false, ifUnset);
