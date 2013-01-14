7f3ccbf03c51b22035ec121043b515bc5e1a6f3e
Sam Bosley <sboz88@gmail.com>
2013-01-14 14:05:27 -0800

    Task row style migration fix

--- astrid/src/com/todoroo/astrid/service/UpgradeService.java
+++ astrid/src/com/todoroo/astrid/service/UpgradeService.java
@@ -258,7 +258,7 @@ public final class UpgradeService {
         if (from < V4_5_1) {
             String key = context.getString(R.string.p_taskRowStyle);
             if (Preferences.isSet(key)) {
-                boolean value = Preferences.getBoolean(key, true);
+                boolean value = Preferences.getBoolean(key, false);
                 Preferences.clear(key);
                 Preferences.setString(R.string.p_taskRowStyle_v2, value ? "1" : "0"); //$NON-NLS-1$ //$NON-NLS-2$
             }
