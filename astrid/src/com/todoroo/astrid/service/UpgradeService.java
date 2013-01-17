358acdb8f9129cb7e00ab5aa4ef07d828f947d3e
Sam Bosley <sboz88@gmail.com>
2013-01-17 15:31:35 -0800

    Changed version number for the next release, updated upgrade message

--- astrid/src/com/todoroo/astrid/service/UpgradeService.java
+++ astrid/src/com/todoroo/astrid/service/UpgradeService.java
@@ -50,7 +50,7 @@ import com.todoroo.astrid.utility.Constants;
 
 public final class UpgradeService {
 
-    public static final int V4_5_1_1 = 293;
+    public static final int V4_5_2 = 293;
     public static final int V4_5_1 = 292;
     public static final int V4_5_0 = 291;
     public static final int V4_4_4_1 = 290;
@@ -265,7 +265,7 @@ public final class UpgradeService {
             }
         }
 
-        if (from < V4_5_1_1) {
+        if (from < V4_5_2) {
             Preferences.setBoolean(R.string.p_simple_input_boxes, true);
         }
     }
@@ -288,9 +288,10 @@ public final class UpgradeService {
         Preferences.clear(AstridPreferences.P_UPGRADE_FROM);
         StringBuilder changeLog = new StringBuilder();
 
-        if (from >= V4_5_1 && from < V4_5_1_1) {
+        if (from >= V4_5_0 && from < V4_5_2) {
             newVersionString(changeLog, "4.5.1.1 (1/17/13)", new String[] {
-                "Fixed a crash that could affect some Google Tasks users"
+                "Fixed a crash that could affect some Google Tasks users",
+                "UI polish"
             });
         }
 
