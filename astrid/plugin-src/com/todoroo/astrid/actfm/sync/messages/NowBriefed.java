8585ea88d9c04f7ca6012c35fb0d30c47e90e1b0
Sam Bosley <sboz88@gmail.com>
2013-03-28 16:37:01 -0700

    Record waiting on mes pushed at in NowBriefed

--- astrid/plugin-src/com/todoroo/astrid/actfm/sync/messages/NowBriefed.java
+++ astrid/plugin-src/com/todoroo/astrid/actfm/sync/messages/NowBriefed.java
@@ -79,6 +79,8 @@ public class NowBriefed<TYPE extends RemoteModel> extends ServerToClientMessage 
                         pushedAtKey = NameMaps.PUSHED_AT_USERS;
                     else if (NameMaps.TABLE_ID_TASK_LIST_METADATA.equals(table))
                         pushedAtKey = NameMaps.PUSHED_AT_TASK_LIST_METADATA;
+                    else if (NameMaps.TABLE_ID_WAITING_ON_ME.equals(table))
+                        pushedAtKey = NameMaps.PUSHED_AT_WAITING_ON_ME;
 
                     if (pushedAtKey != null)
                         Preferences.setLong(pushedAtKey, pushedAt);
