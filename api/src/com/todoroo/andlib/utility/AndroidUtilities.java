1ccad4f613008595cc7eb23446f53586732a99ca
Sam Bosley <sboz88@gmail.com>
2013-04-01 10:52:42 -0700

    Fixed bitmap resize logic

--- api/src/com/todoroo/andlib/utility/AndroidUtilities.java
+++ api/src/com/todoroo/andlib/utility/AndroidUtilities.java
@@ -128,7 +128,7 @@ public class AndroidUtilities {
         Bitmap bitmap = null;
         int tries = 0;
         BitmapFactory.Options opts = new BitmapFactory.Options();
-        while(bitmap == null || ((bitmap.getWidth() > MAX_DIM || bitmap.getHeight() > MAX_DIM) && tries < SAMPLE_SIZES.length)) {
+        while((bitmap == null || (bitmap.getWidth() > MAX_DIM || bitmap.getHeight() > MAX_DIM)) && tries < SAMPLE_SIZES.length) {
             opts.inSampleSize = SAMPLE_SIZES[tries];
             try {
                 bitmap = BitmapFactory.decodeFile(file, opts);
