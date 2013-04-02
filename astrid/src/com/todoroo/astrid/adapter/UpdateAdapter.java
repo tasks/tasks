794b05c7740c95fdd97d8c6c025a90d7cd62d4ea
Sam Bosley <sboz88@gmail.com>
2013-04-02 16:52:06 -0700

    Fixed attachment history display

--- astrid/src/com/todoroo/astrid/adapter/UpdateAdapter.java
+++ astrid/src/com/todoroo/astrid/adapter/UpdateAdapter.java
@@ -484,11 +484,11 @@ public class UpdateAdapter extends CursorAdapter {
 
             } else if (History.COL_ATTACHMENT_ADDED.equals(column) || History.COL_ATTACHMENT_REMOVED.equals(column)) {
                 JSONArray attachmentArray = new JSONArray(newValue);
-                String attachmentName = attachmentArray.getString(1);
+                String attachmentName = attachmentArray.getString(0);
                 if (History.COL_ATTACHMENT_ADDED.equals(column))
-                    result = context.getString(R.string.history_attach_added, item, attachmentName);
+                    result = context.getString(R.string.history_attach_added, attachmentName, item);
                 else
-                    result = context.getString(R.string.history_attach_removed, item, attachmentName);
+                    result = context.getString(R.string.history_attach_removed, attachmentName, item);
             } else if (History.COL_ACKNOWLEDGED.equals(column)) {
                 result = context.getString(R.string.history_acknowledged, item);
             } else if (History.COL_SHARED_WITH.equals(column) || History.COL_UNSHARED_WITH.equals(column)) {
