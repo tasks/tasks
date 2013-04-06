52d6adf56a09b0bc125becec9d199067fe3634dd
Sam Bosley <sboz88@gmail.com>
2013-04-05 17:08:46 -0700

    Handle another login error code

--- astrid/plugin-src/com/todoroo/astrid/actfm/ActFmLoginActivity.java
+++ astrid/plugin-src/com/todoroo/astrid/actfm/ActFmLoginActivity.java
@@ -882,7 +882,7 @@ public class ActFmLoginActivity extends SherlockFragmentActivity {
                             message = getString(R.string.actfm_ALA_error_user_exists);
                         else if ("incorrect_password".equals(code))
                             message = getString(R.string.actfm_ALA_error_wrong_password);
-                        else if ("user_not_found".equals(code))
+                        else if ("user_not_found".equals(code) || "missing_param".equals(code))
                             message = getString(R.string.actfm_ALA_error_user_not_found);
                     }
                 }
