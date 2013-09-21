a73a835bb4cb212a501b745d616b01fc309c9409
Alex Baker <baker.alex@gmail.com>
2013-09-21 13:32:53 -0500

    Update translations
    * Remove unused string arrays from master files
    * Remove unused string arrays from translations
    * French (DianeRouquine)

--- bin/find-unused-strings.rb
+++ bin/find-unused-strings.rb
@@ -5,4 +5,9 @@ files.each do |path|
         result = `git grep R.string.#{match[0]} astrid api` + `git grep string\/#{match[0]} astrid api`
         puts "#{path} - #{match}" if result.empty?
     end
+    file = File.new(path)
+    file.read.scan(/<string-array name="(.*)">/) do |match|
+      result = `git grep R.array.#{match[0]} astrid api` + `git grep array\/#{match[0]} astrid api`
+      puts "#{path} - #{match}" if result.empty?
+    end
 end
