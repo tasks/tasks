4a520aebd43e69b637004784958557d916024ea9
Alex Baker <baker.alex@gmail.com>
2013-09-26 12:17:49 -0500

    Fix save task when pressing back
    Closes #23

--- astrid/src/main/java/com/todoroo/astrid/activity/TaskEditFragment.java
+++ astrid/src/main/java/com/todoroo/astrid/activity/TaskEditFragment.java
@@ -839,7 +839,11 @@ ViewPager.OnPageChangeListener, EditNoteActivity.UpdatesChangedListener {
 
     public boolean onKeyDown(int keyCode) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
-            discardButtonClick();
+            if(title.getText().length() == 0) {
+                discardButtonClick();
+            } else {
+                saveButtonClick();
+            }
             return true;
         }
         return false;
