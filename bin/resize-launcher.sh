da795dfc5a95eb48cb35de22eecfb7909e8fa099
Alex Baker <baker.alex@gmail.com>
2013-10-08 17:11:08 -0500

    Add resize scripts

---
+++ bin/resize-launcher.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+INPUT="$1"
+FILENAME="$2"
+RESOURCES=../astrid/src/main/res
+
+convert -resize 144x144 ${INPUT} ${RESOURCES}/drawable-xxhdpi/${FILENAME}
+convert -resize 96x96 ${INPUT} ${RESOURCES}/drawable-xhdpi/${FILENAME}
+convert -resize 72x72 ${INPUT} ${RESOURCES}/drawable-hdpi/${FILENAME}
+convert -resize 48x48 ${INPUT} ${RESOURCES}/drawable/${FILENAME}
