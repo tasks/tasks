e2268f87205cae9561dd514266326ea0c450f499
Alex Baker <baker.alex@gmail.com>
2013-10-24 17:50:40 -0500

    Add robolectric and gradle android test plugin

---
+++ astrid/src/test/java/org/tasks/SampleTest.java
@@ -0,0 +1,12 @@
+package org.tasks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class SampleTest {
+    @Test
+    public void SomeTest() {
+    }
+}
