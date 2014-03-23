24e93c3c5779959181bc54a9251d11368198fd33
jacsz <js@fen.com.pl>
2014-03-23 18:56:09 +0100

    #280686 Rozszerzenie tabeli Task

--- astrid/src/main/java/com/todoroo/astrid/dao/TaskDao.java
+++ astrid/src/main/java/com/todoroo/astrid/dao/TaskDao.java
@@ -7,7 +7,9 @@ package com.todoroo.astrid.dao;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteConstraintException;
+import android.database.sqlite.SQLiteDatabase;
 
 import com.todoroo.andlib.data.Property;
 import com.todoroo.andlib.data.TodorooCursor;
@@ -386,6 +388,22 @@ public class TaskDao extends RemoteModelDao<Task> {
      */
     private static void afterComplete(Task task) {
         Notifications.cancelNotifications(task.getId());
+    }
+
+    public static void migrateLoggedTime(SQLiteDatabase database){
+        database.beginTransaction();
+
+        Cursor cursor = database.query(Task.TABLE.name, new String[] {Task.ID.name, Task.ELAPSED_SECONDS.name}, null, null, null, null, null);
+        try {
+            while (cursor.moveToNext()){
+                int index = cursor.getColumnIndex(Task.ELAPSED_SECONDS.name);
+                long elapsed = cursor.getLong(index);
+                long id = cursor.getLong(index);
+                //TODO insert new row to table TimeLog
+            }
+        } finally {
+            cursor.close();
+        }
     }
 
 }
