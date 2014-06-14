d402dbdefdfa5a2d400c83885473f91f310e3c8e
Alex Baker <baker.alex@gmail.com>
2014-06-14 01:18:34 -0500

    Update logging

--- astrid/src/main/java/com/todoroo/astrid/backup/TasksXmlExporter.java
+++ astrid/src/main/java/com/todoroo/astrid/backup/TasksXmlExporter.java
@@ -299,7 +299,7 @@ public class TasksXmlExporter {
                 xml.attribute(null, property.name, value);
             } catch (UnsupportedOperationException e) {
                 // didn't read this value, do nothing
-                log.error(e.getMessage(), e);
+                log.debug(e.getMessage(), e);
             } catch (IllegalArgumentException | IOException | IllegalStateException e) {
                 throw new RuntimeException(e);
             }
