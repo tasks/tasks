4779b09031f6d9490ef84b3bbe3400f38b8d8744
Alex Baker <baker.alex@gmail.com>
2014-06-24 15:11:00 -0500

    Catch IllegalStateExceptions in progress callback

--- astrid/src/main/java/org/tasks/sync/IndeterminateProgressBarSyncResultCallback.java
+++ astrid/src/main/java/org/tasks/sync/IndeterminateProgressBarSyncResultCallback.java
@@ -4,7 +4,12 @@ import android.support.v4.app.FragmentActivity;
 
 import com.todoroo.astrid.sync.SyncResultCallback;
 
-public class IndeterminateProgressBarSyncResultCallback implements SyncResultCallback{
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IndeterminateProgressBarSyncResultCallback implements SyncResultCallback {
+
+    private static final Logger log = LoggerFactory.getLogger(IndeterminateProgressBarSyncResultCallback.class);
 
     private final FragmentActivity activity;
     private Runnable onFinished;
@@ -19,8 +24,12 @@ public class IndeterminateProgressBarSyncResultCallback implements SyncResultCal
         activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                activity.setProgressBarIndeterminateVisibility(false);
-                onFinished.run();
+                try {
+                    activity.setProgressBarIndeterminateVisibility(false);
+                    onFinished.run();
+                } catch (IllegalStateException e) {
+                    log.error(e.getMessage(), e);
+                }
             }
         });
     }
@@ -30,7 +39,11 @@ public class IndeterminateProgressBarSyncResultCallback implements SyncResultCal
         activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                activity.setProgressBarIndeterminateVisibility(true);
+                try {
+                    activity.setProgressBarIndeterminateVisibility(true);
+                } catch (IllegalStateException e) {
+                    log.error(e.getMessage(), e);
+                }
             }
         });
 
