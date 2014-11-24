d20e9f4d8e719b013a4214017b2275604efc7311
Alex Baker <baker.alex@gmail.com>
2014-11-24 15:16:31 -0600

    Prevent swipe refresh when moving tasks

---
+++ astrid/src/main/java/org/tasks/ui/DraggableSwipeRefreshLayout.java
@@ -0,0 +1,27 @@
+package org.tasks.ui;
+
+import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import com.todoroo.astrid.utility.Flags;
+
+public class DraggableSwipeRefreshLayout extends SwipeRefreshLayout {
+
+    public DraggableSwipeRefreshLayout(Context context) {
+        super(context);
+    }
+
+    public DraggableSwipeRefreshLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (Flags.check(Flags.TLFP_NO_INTERCEPT_TOUCH)) {
+            return false;
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+}
