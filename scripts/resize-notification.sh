9be08aca0d7dfb94d55c7a24d5238d56bc55cb07
Alex Baker <baker.alex@gmail.com>
2014-11-25 23:57:07 -0600

    New icons

--- scripts/resize-notification.sh
+++ scripts/resize-notification.sh
@@ -2,7 +2,7 @@
 
 INPUT="$1"
 FILENAME="$2"
-RESOURCES=../src/main/res
+RESOURCES=src/main/res
 
 convert -resize 72x72 ${INPUT} ${RESOURCES}/drawable-xxhdpi/${FILENAME}
 convert -resize 48x48 ${INPUT} ${RESOURCES}/drawable-xhdpi/${FILENAME}
