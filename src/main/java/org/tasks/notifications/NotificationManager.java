f9e9b7cd7e0c5fd59bd8f1a8126c256ba6dade00
Alex Baker <baker.alex@gmail.com>
2015-03-16 12:31:08 -0500

    Restore option to disable notifications

--- src/main/java/org/tasks/notifications/NotificationManager.java
+++ src/main/java/org/tasks/notifications/NotificationManager.java
@@ -3,7 +3,9 @@ package org.tasks.notifications;
 import android.app.Notification;
 import android.content.Context;
 
+import org.tasks.R;
 import org.tasks.injection.ForApplication;
+import org.tasks.preferences.Preferences;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -12,9 +14,11 @@ import javax.inject.Singleton;
 public class NotificationManager {
 
     private final android.app.NotificationManager notificationManager;
+    private final Preferences preferences;
 
     @Inject
-    public NotificationManager(@ForApplication Context context) {
+    public NotificationManager(@ForApplication Context context, Preferences preferences) {
+        this.preferences = preferences;
         notificationManager = (android.app.NotificationManager)
                 context.getSystemService(Context.NOTIFICATION_SERVICE);
     }
@@ -24,6 +28,8 @@ public class NotificationManager {
     }
 
     public void notify(int notificationId, Notification notification) {
-        notificationManager.notify(notificationId, notification);
+        if (preferences.getBoolean(R.string.p_rmd_enabled, true)) {
+            notificationManager.notify(notificationId, notification);
+        }
     }
 }
