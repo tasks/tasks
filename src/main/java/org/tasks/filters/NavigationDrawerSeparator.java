f2448ae77649007a2ebe398b369e206cebe7c186
Alex Baker <baker.alex@gmail.com>
2015-12-08 11:21:34 -0600

    Add CREATOR to parcelables

--- src/main/java/org/tasks/filters/NavigationDrawerSeparator.java
+++ src/main/java/org/tasks/filters/NavigationDrawerSeparator.java
@@ -1,5 +1,8 @@
 package org.tasks.filters;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.todoroo.astrid.api.FilterListItem;
 
 public class NavigationDrawerSeparator extends FilterListItem {
@@ -8,4 +11,25 @@ public class NavigationDrawerSeparator extends FilterListItem {
     public Type getItemType() {
         return Type.SEPARATOR;
     }
+
+    public static final Parcelable.Creator<NavigationDrawerSeparator> CREATOR = new Parcelable.Creator<NavigationDrawerSeparator>() {
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public NavigationDrawerSeparator createFromParcel(Parcel source) {
+            NavigationDrawerSeparator navigationDrawerSeparator = new NavigationDrawerSeparator();
+            navigationDrawerSeparator.readFromParcel(source);
+            return navigationDrawerSeparator;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public NavigationDrawerSeparator[] newArray(int size) {
+            return new NavigationDrawerSeparator[size];
+        }
+    };
 }
--- src/main/java/org/tasks/filters/NavigationDrawerSubheader.java
+++ src/main/java/org/tasks/filters/NavigationDrawerSubheader.java
@@ -1,8 +1,15 @@
 package org.tasks.filters;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.todoroo.astrid.api.FilterListItem;
 
 public class NavigationDrawerSubheader extends FilterListItem {
+
+    private NavigationDrawerSubheader() {
+
+    }
 
     public NavigationDrawerSubheader(String listingTitle) {
         this.listingTitle = listingTitle;
@@ -12,4 +19,25 @@ public class NavigationDrawerSubheader extends FilterListItem {
     public Type getItemType() {
         return Type.SUBHEADER;
     }
+
+    public static final Parcelable.Creator<NavigationDrawerSubheader> CREATOR = new Parcelable.Creator<NavigationDrawerSubheader>() {
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public NavigationDrawerSubheader createFromParcel(Parcel source) {
+            NavigationDrawerSubheader navigationDrawerSubheader = new NavigationDrawerSubheader();
+            navigationDrawerSubheader.readFromParcel(source);
+            return navigationDrawerSubheader;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public NavigationDrawerSubheader[] newArray(int size) {
+            return new NavigationDrawerSubheader[size];
+        }
+    };
 }
