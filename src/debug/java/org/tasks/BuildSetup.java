20b62ba15f5177fd82cc9b453a0adba389ed053b
Alex Baker <baker.alex@gmail.com>
2016-06-21 12:17:43 -0500

    Add stetho-timber

--- src/debug/java/org/tasks/BuildSetup.java
+++ src/debug/java/org/tasks/BuildSetup.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 
 import com.facebook.stetho.Stetho;
+import com.facebook.stetho.timber.StethoTree;
 import com.squareup.leakcanary.LeakCanary;
 
 import org.tasks.injection.ForApplication;
@@ -22,6 +23,7 @@ public class BuildSetup {
 
     public void setup() {
         Timber.plant(new Timber.DebugTree());
+        Timber.plant(new StethoTree());
         Stetho.initializeWithDefaults(context);
         LeakCanary.install((Application) context);
     }
