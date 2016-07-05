66e4d4380c266fc72e35f1af6a51e53b0a0ad082
Alex Baker <baker.alex@gmail.com>
2016-07-05 12:11:06 -0500

    Include all active and visible tasks in My Tasks

--- src/main/java/com/todoroo/astrid/core/BuiltInFilterExposer.java
+++ src/main/java/com/todoroo/astrid/core/BuiltInFilterExposer.java
@@ -84,11 +84,7 @@ public final class BuiltInFilterExposer {
      */
     public static Filter getMyTasksFilter(Resources r) {
         return new Filter(r.getString(R.string.BFE_Active),
-                new QueryTemplate().where(
-                        Criterion.and(TaskCriteria.activeAndVisible(),
-                                Criterion.not(Task.ID.in(Query.select(Metadata.TASK).from(Metadata.TABLE).where(
-                                        Criterion.and(MetadataCriteria.withKey(TaskToTagMetadata.KEY),
-                                                TaskToTagMetadata.TAG_NAME.like("x_%", "x"))))))), //$NON-NLS-1$ //$NON-NLS-2$
+                new QueryTemplate().where(TaskCriteria.activeAndVisible()),
                 null);
     }
 
