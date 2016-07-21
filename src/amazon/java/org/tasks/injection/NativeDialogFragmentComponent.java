4bdfbf8ccd723a37dedfa9f8c7847b651599a268
Alex Baker <baker.alex@gmail.com>
2016-07-20 21:57:55 -0500

    Fix lint issues

---
+++ src/amazon/java/org/tasks/injection/NativeDialogFragmentComponent.java
@@ -0,0 +1,8 @@
+package org.tasks.injection;
+
+import dagger.Subcomponent;
+
+@Subcomponent(modules = NativeDialogFragmentModule.class)
+public interface NativeDialogFragmentComponent extends BaseNativeDialogFragmentComponent {
+
+}
