92610623e8abe5a20970fefeb46aa94d18e9d8ee
Alex Baker <alex@tasks.org>
2017-02-06 10:51:54 -0600

    Enable strict mode logging

--- src/debug/java/org/tasks/BuildSetup.java
+++ src/debug/java/org/tasks/BuildSetup.java
@@ -2,6 +2,7 @@ package org.tasks;
 
 import android.app.Application;
 import android.content.Context;
+import android.os.StrictMode;
 
 import com.facebook.stetho.Stetho;
 import com.facebook.stetho.timber.StethoTree;
@@ -26,5 +27,16 @@ public class BuildSetup {
         Timber.plant(new StethoTree());
         Stetho.initializeWithDefaults(context);
         LeakCanary.install((Application) context.getApplicationContext());
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .detectNetwork()
+                .penaltyLog()
+                .build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .build());
     }
 }
