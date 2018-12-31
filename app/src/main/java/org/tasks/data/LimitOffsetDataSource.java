2ca14c8f3118b3929b2a108cd44baf57cb19b403
Bernhard Mähr <git@bmaehr.com>
2018-12-31 05:50:15 +0100

    Reverted merge changes

--- app/src/main/java/org/tasks/data/LimitOffsetDataSource.java
+++ app/src/main/java/org/tasks/data/LimitOffsetDataSource.java
@@ -32,7 +32,7 @@ public class LimitOffsetDataSource extends PositionalDataSource<Task> {
       cursor = mDb.query(mCountQuery, null);
     } catch (Exception e) {
       Timber.e(e);
-      //TODO Tracker.report(e);
+      Tracker.report(e);
       return 0;
     }
     try {
