10a23c76472a5c5955421c795b4f33fc9028ddde
Bernhard Mähr <git@bmaehr.com>
2018-12-31 03:09:49 +0100

    Fixer tests

--- app/src/androidTestAmazon/java/org/tasks/injection/TestComponent.java
+++ app/src/androidTestAmazon/java/org/tasks/injection/TestComponent.java
@@ -1,11 +1,8 @@
 package org.tasks.injection;
 
 import com.todoroo.astrid.dao.Database;
-import com.todoroo.astrid.dao.MetadataDaoTests;
 import com.todoroo.astrid.dao.TaskDaoTests;
 import com.todoroo.astrid.model.TaskTest;
-import com.todoroo.astrid.provider.Astrid3ProviderTests;
-import com.todoroo.astrid.reminders.NotificationTests;
 import com.todoroo.astrid.reminders.ReminderServiceTest;
 import com.todoroo.astrid.repeats.NewRepeatTests;
 import com.todoroo.astrid.service.QuickAddMarkupTest;
@@ -13,6 +10,9 @@ import com.todoroo.astrid.service.TitleParserTest;
 import com.todoroo.astrid.subtasks.SubtasksHelperTest;
 import com.todoroo.astrid.subtasks.SubtasksTestCase;
 import com.todoroo.astrid.sync.NewSyncTestCase;
+import com.todoroo.astrid.alarms.AlarmJobServiceTest;
+import org.tasks.data.DeletionDaoTests;
+import com.todoroo.astrid.repeats.RepeatTaskHelperTest;
 import dagger.Component;
 import org.tasks.jobs.BackupServiceTests;
 
@@ -28,10 +28,6 @@ public interface TestComponent {
 
   void inject(TaskDaoTests taskDaoTests);
 
-  void inject(MetadataDaoTests metadataDaoTests);
-
-  void inject(Astrid3ProviderTests astrid3ProviderTests);
-
   void inject(NewSyncTestCase newSyncTestCase);
 
   void inject(SubtasksTestCase subtasksTestCase);
@@ -46,6 +42,10 @@ public interface TestComponent {
 
   void inject(BackupServiceTests backupServiceTests);
 
-  NotificationTests.NotificationTestsComponent plus(
-      NotificationTests.NotificationTestsModule notificationTestsModule);
+  void inject(AlarmJobServiceTest alarmJobServiceTest);
+
+  void inject(DeletionDaoTests deletionDaoTests);
+
+  void inject(RepeatTaskHelperTest repeatTaskHelperTest);
+
 }
