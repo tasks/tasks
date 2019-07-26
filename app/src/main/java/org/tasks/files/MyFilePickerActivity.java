b0bd5a2b5b5163717aaaf7592d97aa6161dcfcdb
Alex Baker <alex@tasks.org>
2019-07-26 16:39:54 -0500

    Remove asContentBackground

--- app/src/main/java/org/tasks/files/MyFilePickerActivity.java
+++ app/src/main/java/org/tasks/files/MyFilePickerActivity.java
@@ -5,7 +5,6 @@ import static org.tasks.preferences.ResourceResolver.getData;
 import android.os.Bundle;
 import com.nononsenseapps.filepicker.FilePickerActivity;
 import javax.inject.Inject;
-import org.tasks.R;
 import org.tasks.injection.ActivityModule;
 import org.tasks.injection.InjectingApplication;
 import org.tasks.themes.Theme;
@@ -23,6 +22,5 @@ public class MyFilePickerActivity extends FilePickerActivity {
     theme.applyThemeAndStatusBarColor(this, getDelegate());
     setTitle(null);
     super.onCreate(savedInstanceState);
-    getWindow().getDecorView().setBackgroundColor(getData(this, R.attr.asContentBackground));
   }
 }
