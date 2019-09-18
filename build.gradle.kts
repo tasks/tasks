2f0d3e885053289d8d1dc59315b4567eca0e6395
Alex Baker <alex@tasks.org>
2019-09-18 15:58:21 -0500

    Update google-services and fabric

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,9 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:3.5.0")
-        classpath("com.google.gms:google-services:4.3.0")
+        classpath("com.google.gms:google-services:4.3.2")
         // https://docs.fabric.io/android/changelog.html#fabric-gradle-plugin
-        classpath("io.fabric.tools:gradle:1.29.0")
+        classpath("io.fabric.tools:gradle:1.31.0")
         classpath("com.github.ben-manes:gradle-versions-plugin:0.21.0")
         classpath("com.cookpad.android.licensetools:license-tools-plugin:1.7.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
