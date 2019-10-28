f14044a3b5c861ea58fa124d4c0034761c97e1ed
Alex Baker <alex@tasks.org>
2019-10-28 18:30:50 -0500

    Dagger 2.25.2

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ object Versions {
     const val kotlin = "1.3.40"
     const val targetSdk = 29
     const val minSdk = 16
-    const val dagger = "2.23.2"
+    const val dagger = "2.25.2"
     const val butterknife = "10.1.0"
     const val work = "2.0.1"
     const val leakcanary = "1.6.3"
