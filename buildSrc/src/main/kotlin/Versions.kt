4d89b606a24e35d40332a665ca2c73e6b592db94
Alex Baker <alex@tasks.org>
2019-11-27 11:29:39 -0600

    OkHttp 3.12.6

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -9,6 +9,6 @@ object Versions {
     const val room = "2.2.2"
     const val crashlytics = "2.10.1"
     const val firebase = "16.0.9"
-    const val okhttp = "3.12.3" // 3.13 minSdk is 21
+    const val okhttp = "3.12.6" // 3.13 minSdk is 21
     const val flipper = "0.28.0"
 }
