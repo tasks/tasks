1e33549d44f2b967f9bd4fe35e6d63087deedda8
Alex Baker <alex@tasks.org>
2020-01-02 13:53:56 -0600

    LeakCanary 2.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,7 +5,7 @@ object Versions {
     const val dagger = "2.25.2"
     const val butterknife = "10.2.0"
     const val work = "2.2.0"
-    const val leakcanary = "2.0-beta-5"
+    const val leakcanary = "2.1"
     const val room = "2.2.2"
     const val crashlytics = "2.10.1"
     const val firebase = "16.0.9"
