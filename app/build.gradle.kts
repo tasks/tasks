058377f94bc00f68be38467cc886b2b77c3435fe
Alex Baker <alex@tasks.org>
2020-02-28 12:15:58 -0600

    Enable room incremental annotation proccesor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ android {
         kapt {
             arguments {
                 arg("room.schemaLocation", "$projectDir/schemas")
+                arg("room.incremental", "true")
             }
         }
     }
