bfd794640ac46a1dae5e279309e26e061bf213da
Alex Baker <alex@tasks.org>
2020-02-28 16:07:17 -0600

    Fix purchase dialog button color

--- app/src/main/java/org/tasks/billing/PurchaseActivity.java
+++ app/src/main/java/org/tasks/billing/PurchaseActivity.java
@@ -7,16 +7,20 @@ import androidx.fragment.app.FragmentManager;
 import javax.inject.Inject;
 import org.tasks.injection.ActivityComponent;
 import org.tasks.injection.InjectingAppCompatActivity;
+import org.tasks.themes.ThemeAccent;
 
 public class PurchaseActivity extends InjectingAppCompatActivity {
 
   private static final String FRAG_TAG_PURCHASE = "frag_tag_purchase";
 
   @Inject Inventory inventory;
+  @Inject ThemeAccent themeAccent;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    themeAccent.applyStyle(getTheme());
 
     FragmentManager fragmentManager = getSupportFragmentManager();
     PurchaseDialog dialog = (PurchaseDialog) fragmentManager.findFragmentByTag(FRAG_TAG_PURCHASE);
