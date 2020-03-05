e0d4072768d47ae590e145578c637f48e2a468d2
Alex Baker <alex@tasks.org>
2020-03-05 16:09:58 -0600

    Add link to RFC

--- app/src/main/java/org/tasks/caldav/CaldavConverter.java
+++ app/src/main/java/org/tasks/caldav/CaldavConverter.java
@@ -82,6 +82,7 @@ public class CaldavConverter {
   }
 
   private static @Priority int fromRemote(int remotePriority) {
+    // https://tools.ietf.org/html/rfc5545#section-3.8.1.9
     if (remotePriority == 0) {
       return Priority.NONE;
     }
