92b2749d62c4b8b2692b7aa488ea00bdb19d2aa5
Alex Baker <alex@tasks.org>
2020-04-01 13:15:06 -0500

    Update third party dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,14 +1,14 @@
 object Versions {
-    const val kotlin = "1.3.70"
+    const val kotlin = "1.3.71"
     const val targetSdk = 29
     const val minSdk = 19
-    const val dagger = "2.26"
+    const val dagger = "2.27"
     const val butterknife = "10.2.1"
     const val work = "2.3.4"
     const val leakcanary = "2.2"
     const val room = "2.2.5"
     const val crashlytics = "2.10.1"
-    const val firebase = "17.2.2"
+    const val firebase = "17.3.0"
     const val okhttp = "3.12.6" // 3.13 minSdk is 21
-    const val flipper = "0.31.2"
+    const val flipper = "0.35.0"
 }
