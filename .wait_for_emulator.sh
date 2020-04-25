70011ee67a5dc76b94616ae9b577e968c28f6f0d
Alex Baker <alex@tasks.org>
2020-04-25 09:02:27 -0500

    New travis config

--- .wait_for_emulator.sh
+++ .wait_for_emulator.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+set +e
+
 bootanim=""
 failcounter=0
-until [[ "$bootanim" =~ "stopped" ]] || [[ "$bootanim" =~ "running" ]]; do
-   bootanim=`adb -e shell getprop init.svc.bootanim 2>&1`
-   echo "$bootanim"
-   if [[ "$bootanim" =~ "not found" ]]; then
-      let "failcounter += 1"
-      if [[ $failcounter -gt 3 ]]; then
-        echo "Failed to start emulator"
-        exit 1
-      fi
-   fi
-   sleep 1
+timeout_in_sec=600 # 10 minutes
+
+until [[ "$bootanim" =~ "stopped" ]]; do
+  bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
+  if [[ "$bootanim" =~ "device not found" || "$bootanim" =~ "device offline"
+    || "$bootanim" =~ "running" || "$bootanim" =~  "error: no emulators found" ]]; then
+    let "failcounter += 1"
+    echo "Waiting for emulator to start"
+    if [[ $failcounter -gt timeout_in_sec ]]; then
+      echo "Timeout ($timeout_in_sec seconds) reached; failed to start emulator"
+      exit 1
+    fi
+  fi
+  sleep 1
 done
-echo "Done"
+
+echo "Emulator is ready"
+
