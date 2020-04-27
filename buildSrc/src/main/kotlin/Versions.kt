0d0bcbe64bf4184de7edce6e6f3b0ccdc7fc80e9
Alex Baker <alex@tasks.org>
2020-04-27 16:11:51 -0500

    Update flipper and firebase

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -8,7 +8,7 @@ object Versions {
     const val leakcanary = "2.2"
     const val room = "2.2.5"
     const val crashlytics = "2.10.1"
-    const val firebase = "17.3.0"
+    const val firebase = "17.4.0"
     const val okhttp = "3.12.10" // 3.13 minSdk is 21
-    const val flipper = "0.38.0"
+    const val flipper = "0.39.0"
 }
