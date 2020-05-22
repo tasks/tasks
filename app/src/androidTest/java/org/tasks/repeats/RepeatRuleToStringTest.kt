d96a4579a3553521278ffa991bee018c11ad817f
Alex Baker <alex@tasks.org>
2020-05-22 07:56:29 -0500

    Update translation in test

--- app/src/androidTest/java/org/tasks/repeats/RepeatRuleToStringTest.kt
+++ app/src/androidTest/java/org/tasks/repeats/RepeatRuleToStringTest.kt
@@ -38,7 +38,7 @@ class RepeatRuleToStringTest {
     @Test
     fun useLocaleForDays() {
         assertEquals(
-                "Wiederhole wöchentlich am Sa., So.",
+                "Wiederholt sich wöchentlich am Sa., So.",
                 toString("de", "RRULE:FREQ=WEEKLY;INTERVAL=1;BYDAY=SA,SU"))
     }
 
