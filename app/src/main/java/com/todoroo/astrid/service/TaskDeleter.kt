0388422553ac381138d47b21e0bcf7e079556a49
Alex Baker <alex@tasks.org>
2020-06-03 13:24:37 -0500

    Convert TaskDeleter to Kotlin

---
+++ app/src/main/java/com/todoroo/astrid/service/TaskDeleter.kt
@@ -0,0 +1,81 @@
+package com.todoroo.astrid.service
+
+import com.todoroo.astrid.api.Filter
+import com.todoroo.astrid.dao.TaskDao
+import com.todoroo.astrid.data.Task
+import kotlinx.collections.immutable.persistentListOf
+import org.tasks.LocalBroadcastManager
+import org.tasks.data.*
+import org.tasks.db.DbUtils
+import org.tasks.db.QueryUtils
+import org.tasks.jobs.WorkManager
+import org.tasks.preferences.Preferences
+import java.util.*
+import javax.inject.Inject
+
+class TaskDeleter @Inject constructor(
+        private val deletionDao: DeletionDao,
+        private val workManager: WorkManager,
+        private val taskDao: TaskDao,
+        private val localBroadcastManager: LocalBroadcastManager,
+        private val googleTaskDao: GoogleTaskDao,
+        private val preferences: Preferences) {
+
+    fun markDeleted(item: Task) = markDeleted(persistentListOf(item.id))
+
+    fun markDeleted(taskIds: List<Long>): List<Task> {
+        val ids: MutableSet<Long> = HashSet(taskIds)
+        ids.addAll(DbUtils.collect(taskIds) { googleTaskDao.getChildren(it!!) })
+        ids.addAll(DbUtils.collect(taskIds) { taskDao.getChildren(it!!) })
+        deletionDao.markDeleted(ids)
+        workManager.cleanup(ids)
+        workManager.sync(false)
+        localBroadcastManager.broadcastRefresh()
+        return DbUtils.collect(ids) { taskDao.fetch(it!!) }
+    }
+
+    fun clearCompleted(filter: Filter): Int {
+        val deleteFilter = Filter(null, null)
+        deleteFilter.setFilterQueryOverride(
+                QueryUtils.removeOrder(QueryUtils.showHiddenAndCompleted(filter.originalSqlQuery)))
+        val completed = taskDao.fetchTasks(preferences, deleteFilter)
+                .filter(TaskContainer::isCompleted)
+                .map(TaskContainer::getId)
+        markDeleted(completed)
+        return completed.size
+    }
+
+    fun delete(task: Task) = delete(task.id)
+
+    fun delete(task: Long) = delete(persistentListOf(task))
+
+    fun delete(tasks: List<Long>) {
+        deletionDao.delete(tasks)
+        workManager.cleanup(tasks)
+        localBroadcastManager.broadcastRefresh()
+    }
+
+    fun delete(list: GoogleTaskList) {
+        val tasks = deletionDao.delete(list)
+        delete(tasks)
+        localBroadcastManager.broadcastRefreshList()
+    }
+
+    fun delete(list: GoogleTaskAccount) {
+        val tasks = deletionDao.delete(list)
+        delete(tasks)
+        localBroadcastManager.broadcastRefreshList()
+    }
+
+    fun delete(list: CaldavCalendar) {
+        val tasks = deletionDao.delete(list)
+        delete(tasks)
+        localBroadcastManager.broadcastRefreshList()
+    }
+
+    fun delete(list: CaldavAccount) {
+        val tasks = deletionDao.delete(list)
+        delete(tasks)
+        localBroadcastManager.broadcastRefreshList()
+    }
+}
