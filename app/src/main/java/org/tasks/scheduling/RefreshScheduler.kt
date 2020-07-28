b44150c7f3e30fa8857b049b9030ea51886464e8
Alex Baker <alex@tasks.org>
2020-07-28 10:29:56 -0500

    Convert RefreshScheduler to Kotlin

---
+++ app/src/main/java/org/tasks/scheduling/RefreshScheduler.kt
@@ -0,0 +1,63 @@
+package org.tasks.scheduling
+
+import com.todoroo.andlib.utility.DateUtilities
+import com.todoroo.astrid.dao.TaskDao
+import com.todoroo.astrid.data.Task
+import kotlinx.collections.immutable.toImmutableList
+import org.tasks.R
+import org.tasks.jobs.WorkManager
+import org.tasks.preferences.Preferences
+import org.tasks.time.DateTimeUtils
+import java.util.*
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class RefreshScheduler @Inject internal constructor(
+        private val preferences: Preferences,
+        private val workManager: WorkManager,
+        private val taskDao: TaskDao) {
+
+    private val jobs: SortedSet<Long> = TreeSet()
+
+    @Synchronized
+    suspend fun scheduleAll() {
+        for (task in taskDao.needsRefresh()) {
+            scheduleRefresh(task)
+        }
+    }
+
+    @Synchronized
+    fun scheduleRefresh(task: Task) {
+        if (task.isCompleted
+                && preferences.getBoolean(R.string.p_temporarily_show_completed_tasks, false)) {
+            scheduleRefresh(task.completionDate + DateUtilities.ONE_MINUTE)
+        } else if (task.hasDueDate()) {
+            scheduleRefresh(task.dueDate)
+        }
+        if (task.hasHideUntilDate()) {
+            scheduleRefresh(task.hideUntil)
+        }
+    }
+
+    @Synchronized
+    fun scheduleNext() {
+        val lapsed = jobs.headSet(DateTimeUtils.currentTimeMillis() + 1).toImmutableList()
+        jobs.removeAll(lapsed)
+        if (!jobs.isEmpty()) {
+            workManager.scheduleRefresh(jobs.first())
+        }
+    }
+
+    private fun scheduleRefresh(timestamp: Long) {
+        val now = DateTimeUtils.currentTimeMillis()
+        if (now < timestamp) {
+            val upcoming = jobs.tailSet(now)
+            val reschedule = upcoming.isEmpty() || timestamp < upcoming.first()
+            jobs.add(timestamp)
+            if (reschedule) {
+                scheduleNext()
+            }
+        }
+    }
+}
