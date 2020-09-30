6b3cb96848542d61867d4b5b00ae681964b7c78e
Alex Baker <alex@tasks.org>
2020-09-30 12:43:09 -0500

    Convert DateTimeUtils to Kotlin

---
+++ app/src/main/java/org/tasks/time/DateTimeUtils.kt
@@ -0,0 +1,40 @@
+package org.tasks.time
+
+import android.annotation.SuppressLint
+import org.tasks.BuildConfig
+import java.util.*
+
+object DateTimeUtils {
+    private val SYSTEM_MILLIS_PROVIDER = SystemMillisProvider()
+
+    @Volatile
+    private var MILLIS_PROVIDER: MillisProvider = SYSTEM_MILLIS_PROVIDER
+
+    @JvmStatic
+    fun currentTimeMillis(): Long {
+        return MILLIS_PROVIDER.millis
+    }
+
+    fun setCurrentMillisFixed(millis: Long) {
+        MILLIS_PROVIDER = FixedMillisProvider(millis)
+    }
+
+    fun setCurrentMillisSystem() {
+        MILLIS_PROVIDER = SYSTEM_MILLIS_PROVIDER
+    }
+
+    fun printTimestamp(timestamp: Long): String {
+        return if (BuildConfig.DEBUG) Date(timestamp).toString() else timestamp.toString()
+    }
+
+    @SuppressLint("DefaultLocale")
+    fun printDuration(millis: Long): String {
+        return if (BuildConfig.DEBUG) {
+            val seconds = millis / 1000
+            String.format(
+                    "%dh %dm %ds", seconds / 3600L, (seconds % 3600L / 60L).toInt(), (seconds % 60L).toInt())
+        } else {
+            millis.toString()
+        }
+    }
+}
