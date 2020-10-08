1bf64ffec2dca1937c6f42166400518be7334d88
Alex Baker <alex@tasks.org>
2020-10-08 16:43:11 -0500

    Convert AppWidgetManager to Kotlin

---
+++ app/src/main/java/org/tasks/widget/AppWidgetManager.kt
@@ -0,0 +1,35 @@
+package org.tasks.widget
+
+import android.appwidget.AppWidgetManager
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import org.tasks.R
+import org.tasks.widget.TasksWidget
+import javax.inject.Inject
+
+class AppWidgetManager @Inject constructor(
+        @param:ApplicationContext private val context: Context
+) {
+    private val appWidgetManager: AppWidgetManager? = AppWidgetManager.getInstance(context)
+
+    val widgetIds: IntArray
+        get() = appWidgetManager
+                ?.getAppWidgetIds(ComponentName(context, TasksWidget::class.java))
+                ?: intArrayOf()
+
+    fun reconfigureWidgets(vararg appWidgetIds: Int) {
+        val intent = Intent(context, TasksWidget::class.java)
+        intent.action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
+        intent.putExtra(
+                AppWidgetManager.EXTRA_APPWIDGET_IDS,
+                appWidgetIds.takeIf { it.isNotEmpty() } ?: widgetIds)
+        context.sendBroadcast(intent)
+        updateWidgets()
+    }
+
+    fun updateWidgets() {
+        appWidgetManager?.notifyAppWidgetViewDataChanged(widgetIds, R.id.list_view)
+    }
+}
