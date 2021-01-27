6cb4ff4aa61b9e257ec4926b3b9bbf230fe89805
Alex Baker <alex@tasks.org>
2021-01-27 17:46:36 -0600

    Fix licenses and generic build

--- app/src/generic/java/org/tasks/injection/LocationModule.kt
+++ app/src/generic/java/org/tasks/injection/LocationModule.kt
@@ -5,18 +5,20 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ActivityScoped
+import dagger.hilt.android.scopes.ViewModelScoped
 import org.tasks.location.MapFragment
 import org.tasks.location.MapboxMapFragment
 import org.tasks.location.MapboxSearchProvider
 import org.tasks.location.PlaceSearchProvider
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(ActivityComponent::class, ViewModelComponent::class)
 class LocationModule {
     @Provides
-    @ActivityScoped
+    @ViewModelScoped
     fun getPlaceSearchProvider(@ApplicationContext context: Context): PlaceSearchProvider {
         return MapboxSearchProvider(context)
     }
