379bf3856f494a53328f8acd8a93b753e39619bf
Alex Baker <alex@tasks.org>
2021-02-23 15:57:18 -0600

    Android Studio 4.2 Beta 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,15 +6,15 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.2")
+        classpath("com.android.tools.build:gradle:4.2.0-beta05")
         classpath("com.google.gms:google-services:4.3.5")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.4.1")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.5.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
         classpath("com.google.dagger:hilt-android-gradle-plugin:${Versions.hilt}")
     }
 }
 
 tasks.getByName<Wrapper>("wrapper") {
-    gradleVersion = "6.1.1"
+    gradleVersion = "6.7.1"
     distributionType = Wrapper.DistributionType.ALL
 }
