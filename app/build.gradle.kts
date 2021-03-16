486b8ec1a108f194266da15cb070cc9ed82c634b
Alex Baker <alex@tasks.org>
2021-03-16 08:07:28 -0500

    Google Drive v3-rev20210228-1.31.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -219,7 +219,7 @@ dependencies {
     implementation("com.wdullaer:materialdatetimepicker:4.2.3")
     implementation("me.leolin:ShortcutBadger:1.1.22@aar")
     implementation("com.google.apis:google-api-services-tasks:v1-rev20210109-1.31.0")
-    implementation("com.google.apis:google-api-services-drive:v3-rev20210207-1.31.0")
+    implementation("com.google.apis:google-api-services-drive:v3-rev20210228-1.31.0")
     implementation("com.google.auth:google-auth-library-oauth2-http:0.24.0")
     implementation("androidx.work:work-runtime:${Versions.work}")
     implementation("androidx.work:work-runtime-ktx:${Versions.work}")
