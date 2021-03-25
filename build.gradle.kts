b3c45f54d34742cc11c83daea3667260aca8aeee
Alex Baker <alex@tasks.org>
2021-03-25 12:55:43 -0500

    firebase-crashlytics-gradle 2.5.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.0-alpha12")
         classpath("com.google.gms:google-services:4.3.5")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.5.1")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.5.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
         classpath("com.google.dagger:hilt-android-gradle-plugin:${Versions.hilt}")
     }
