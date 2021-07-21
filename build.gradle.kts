5d34496efceacfa97332c41e36c90723ae162aeb
Alex Baker <alex@tasks.org>
2021-07-21 10:51:18 -0500

    Android Studio Arctic Fox rc01

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-beta05")
+        classpath("com.android.tools.build:gradle:7.0.0-rc01")
         classpath("com.google.gms:google-services:4.3.8")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.5.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
