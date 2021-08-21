fd1c8657f051eabd5b64fab188868ab66afa8505
Alex Baker <alex@tasks.org>
2021-08-21 13:28:07 -0500

    Timber 5.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -206,7 +206,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.0-beta02")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("androidx.preference:preference:1.1.1")
-    implementation("com.jakewharton.timber:timber:4.7.1")
+    implementation("com.jakewharton.timber:timber:5.0.1")
     implementation("com.google.android.apps.dashclock:dashclock-api:2.0.0")
     implementation("com.twofortyfouram:android-plugin-api-for-locale:1.0.2") {
         isTransitive = false
