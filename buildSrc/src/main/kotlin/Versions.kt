599faa660cb2644856ec8e37de406b712ad43a87
Alex Baker <alex@tasks.org>
2022-01-04 22:36:40 -0600

    Update flipper and leak canary

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,13 +4,13 @@ object Versions {
     const val targetSdk = 31
     const val minSdk = 24
     const val work = "2.7.0"
-    const val leakcanary = "2.7"
+    const val leakcanary = "2.8"
     const val room = "2.4.0-beta01"
     const val crashlytics = "18.2.1"
     const val analytics = "19.0.1"
     const val remote_config = "21.0.1"
     const val okhttp = "4.8.1"
-    const val flipper = "0.110.0"
+    const val flipper = "0.127.0"
     const val mockito = "3.9.0"
     const val androidx_test = "1.3.0"
     const val make_it_easy = "4.0.1"
