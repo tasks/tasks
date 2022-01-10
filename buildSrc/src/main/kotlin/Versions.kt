898960a7dc0c1571ce9bcc7f968ecaa8976a8f4c
Alex Baker <alex@tasks.org>
2022-01-10 12:58:01 -0600

    Leak Canary 2.8.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,7 +4,7 @@ object Versions {
     const val targetSdk = 31
     const val minSdk = 24
     const val work = "2.7.0"
-    const val leakcanary = "2.8"
+    const val leakcanary = "2.8.1"
     const val room = "2.4.0-beta01"
     const val crashlytics = "18.2.1"
     const val analytics = "19.0.1"
