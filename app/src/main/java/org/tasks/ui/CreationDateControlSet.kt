9b3f5a0c650e7b76b98f644a0c6e6dd08e9867f2
Alex Baker <alex@tasks.org>
2022-07-11 23:15:27 -0500

    Convert CreationDateControlSet to compose

--- app/src/main/java/org/tasks/ui/CreationDateControlSet.kt
+++ app/src/main/java/org/tasks/ui/CreationDateControlSet.kt
@@ -1,33 +1,44 @@
 package org.tasks.ui
 
-import android.view.ViewGroup
-import android.widget.TextView
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import dagger.hilt.android.AndroidEntryPoint
 import org.tasks.R
-import org.tasks.databinding.ControlSetCreationDateBinding
 import org.tasks.preferences.Preferences
 import java.text.SimpleDateFormat
 import java.util.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class CreationDateControlSet : TaskEditControlFragment() {
+class CreationDateControlSet : TaskEditControlComposeFragment() {
     @Inject lateinit var locale: Locale
-    @Inject lateinit var preferences: Preferences
 
-    private lateinit var date: TextView
-
-    override fun bind(parent: ViewGroup?) =
-        ControlSetCreationDateBinding.inflate(layoutInflater, parent, true).let {
-            val formatter = SimpleDateFormat( "yyyy-MM-dd HH:mm", locale)
-            date = it.creationDate
-            var dateRecords = context?.getString( R.string.sort_created_group, formatter.format( viewModel.creationDate!!)) + "\n" +
-                    context?.getString( R.string.sort_modified_group, formatter.format( viewModel.modificationDate!!))
-            if (viewModel.completionDate!! != 0L)
-                dateRecords += "\n" + context?.getString( R.string.sort_completion_group, formatter.format( viewModel.completionDate!!))
-            date.text = dateRecords
-            it.root
+    @Composable
+    override fun Body() {
+        Column(modifier = Modifier.padding(vertical = 20.dp)) {
+            val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm", locale)
+            viewModel.creationDate?.let {
+                Text(
+                    text = stringResource(id = R.string.sort_created_group, formatter.format(it))
+                )
+            }
+            viewModel.modificationDate?.let {
+                Text(
+                    text = stringResource(id = R.string.sort_modified_group, formatter.format(it))
+                )
+            }
+            viewModel.completionDate?.takeIf { it > 0 }?.let {
+                Text(
+                    text = stringResource(id = R.string.sort_completion_group, formatter.format(it))
+                )
+            }
         }
+    }
 
     override val icon = R.drawable.ic_outline_info_24px
 
