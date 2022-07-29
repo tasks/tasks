7b9b71dd67a6cc7ad2026d67dd4874f8b4e2792c
Alex Baker <alex@tasks.org>
2022-07-29 09:40:30 -0500

    Fill max width for task edit rows

--- app/src/main/java/org/tasks/compose/TaskEditRow.kt
+++ app/src/main/java/org/tasks/compose/TaskEditRow.kt
@@ -2,6 +2,7 @@ package org.tasks.compose
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ContentAlpha
 import androidx.compose.runtime.Composable
@@ -29,6 +30,7 @@ fun TaskEditRow(
     onClick: (() -> Unit)? = null,
 ) {
     Row(modifier = Modifier
+        .fillMaxWidth()
         .clickable(
             enabled = onClick != null,
             onClick = { onClick?.invoke() }
