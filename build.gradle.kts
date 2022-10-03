28e92e00c9d416f661a6f099dcea8d92c0c4699f
Alex Baker <alex@tasks.org>
2022-10-03 18:27:43 -0500

    Update google and firebase plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:7.3.0")
-        classpath("com.google.gms:google-services:4.3.13")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.1")
+        classpath("com.google.gms:google-services:4.3.14")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
         classpath("com.google.dagger:hilt-android-gradle-plugin:${Versions.hilt}")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.5")
