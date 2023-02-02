1eba02604defcccf1ba7ee1c82a412e496109594
Alex Baker <alex@tasks.org>
2023-02-01 23:44:59 -0600

    Update some libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,11 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:7.4.0")
-        classpath("com.google.gms:google-services:4.3.14")
+        classpath("com.google.gms:google-services:4.3.15")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
         classpath("com.google.dagger:hilt-android-gradle-plugin:${Versions.hilt}")
-        classpath("com.google.android.gms:oss-licenses-plugin:0.10.5")
+        classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
     }
 }
 
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,13 +4,13 @@ object Versions {
     const val targetSdk = 33
     const val minSdk = 24
     const val work = "2.7.1"
-    const val leakcanary = "2.9.1"
+    const val leakcanary = "2.10"
     const val room = "2.4.2"
     const val crashlytics = "18.2.11"
     const val analytics = "20.1.0"
     const val remote_config = "21.0.1"
     const val okhttp = "4.10.0"
-    const val flipper = "0.149.0"
+    const val flipper = "0.177.0"
     const val mockito = "3.9.0"
     const val androidx_test = "1.3.0"
     const val make_it_easy = "4.0.1"
