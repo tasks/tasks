b525e8cab32e68309f7962dcef6cf5de7cc13174
kmj-99 <nex1223@naver.com>
2023-10-03 14:53:15 +0900

    Refactor: Change deprecated code in Fragment onAttach

--- app/src/main/java/com/todoroo/astrid/activity/TaskListFragment.kt
+++ app/src/main/java/com/todoroo/astrid/activity/TaskListFragment.kt
@@ -223,8 +223,8 @@ class TaskListFragment : Fragment(), OnRefreshListener, Toolbar.OnMenuItemClickL
         }
     }
 
-    override fun onAttach(activity: Activity) {
-        super.onAttach(activity)
+    override fun onAttach(context: Context) {
+        super.onAttach(requireContext())
         callbacks = activity as TaskListFragmentCallbackHandler
     }
 
