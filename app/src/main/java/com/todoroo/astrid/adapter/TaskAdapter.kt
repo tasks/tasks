tasks/tasks

commit 775289b058f29d28ea117bb088d6e10889b3259f

Update modification timestamp logic (#2585)

app/src/main/java/org/tasks/data/TaskDao.kt
@@ -188,15 +188,19 @@ FROM recursive_tasks
     @Insert
     abstract suspend fun insert(task: Task): Long
 
-    suspend fun update(task: Task, original: Task? = null): Boolean {
-        if (!task.insignificantChange(original)) {
-            task.modificationDate = now()
-        }
-        return updateInternal(task) == 1
-    }
+    suspend fun update(task: Task, original: Task? = null): Task =
+        task
+            .copy(
+                modificationDate = when {
+                    original?.let { task.significantChange(it) } == true -> now()
+                    task.modificationDate == 0L -> task.creationDate
+                    else -> task.modificationDate
+                }
+            )
+            .also { updateInternal(it) }
 
     @Update
-    internal abstract suspend fun updateInternal(task: Task): Int
+    internal abstract suspend fun updateInternal(task: Task)
 
     suspend fun createNew(task: Task): Long {
         task.id = NO_ID

app/src/main/java/com/todoroo/astrid/dao/TaskDao.kt
@@ -106,9 +106,8 @@ class TaskDao @Inject constructor(
         }
 
     suspend fun save(task: Task, original: Task?) {
-        if (taskDao.update(task, original)) {
-            afterUpdate(task, original)
-        }
+        val updated = taskDao.update(task, original)
+        afterUpdate(updated, original)
     }
 
     private suspend fun afterUpdate(task: Task, original: Task?) {

app/src/main/java/com/todoroo/astrid/data/Task.kt
@@ -187,31 +187,25 @@ data class Task(
     val isNew: Boolean
         get() = id == NO_ID
 
-    fun insignificantChange(task: Task?): Boolean {
-        if (this === task) {
-            return true
-        }
-        return if (task == null) {
-            false
-        } else id == task.id
-                && title == task.title
-                && priority == task.priority
-                && dueDate == task.dueDate
-                && hideUntil == task.hideUntil
-                && creationDate == task.creationDate
-                && modificationDate == task.modificationDate
-                && completionDate == task.completionDate
-                && deletionDate == task.deletionDate
-                && notes == task.notes
-                && estimatedSeconds == task.estimatedSeconds
-                && elapsedSeconds == task.elapsedSeconds
-                && ringFlags == task.ringFlags
-                && recurrence == task.recurrence
-                && calendarURI == task.calendarURI
-                && parent == task.parent
-                && remoteId == task.remoteId
-                && order == task.order
-    }
+    fun significantChange(task: Task): Boolean =
+        id != task.id
+                || title != task.title
+                || priority != task.priority
+                || dueDate != task.dueDate
+                || hideUntil != task.hideUntil
+                || creationDate != task.creationDate
+                || modificationDate != task.modificationDate
+                || completionDate != task.completionDate
+                || deletionDate != task.deletionDate
+                || notes != task.notes
+                || estimatedSeconds != task.estimatedSeconds
+                || elapsedSeconds != task.elapsedSeconds
+                || ringFlags != task.ringFlags
+                || recurrence != task.recurrence
+                || calendarURI != task.calendarURI
+                || parent != task.parent
+                || remoteId != task.remoteId
+                || order != task.order
 
     fun googleTaskUpToDate(original: Task?): Boolean {
         if (this === original) {

app/src/main/java/org/tasks/caldav/iCalendar.kt
@@ -215,7 +215,7 @@ class iCalendar @Inject constructor(
                     `object` = obj
                 )
         val isNew = caldavTask.id == com.todoroo.astrid.data.Task.NO_ID
-        val dirty = task.modificationDate > caldavTask.lastSync || caldavTask.lastSync == 0L
+        val dirty = !isNew && task.modificationDate > caldavTask.lastSync
         val local = vtodoCache.getVtodo(calendar, caldavTask)?.let { fromVtodo(it) }
         task.applyRemote(remote, local)
         caldavTask.applyRemote(remote, local)
@@ -263,7 +263,11 @@ class iCalendar @Inject constructor(
 
         task.suppressSync()
         task.suppressRefresh()
-        taskDao.save(task)
+        if (isNew) {
+            taskDao.save(task, null)
+        } else {
+            taskDao.save(task)
+        }
         vtodoCache.putVtodo(calendar, caldavTask, vtodo)
         caldavTask.etag = eTag
         if (!dirty) {

app/src/main/java/org/tasks/caldav/iCalendarMerge.kt
@@ -2,6 +2,7 @@ package org.tasks.caldav
 
 import at.bitfire.ical4android.Task
 import com.todoroo.andlib.utility.DateUtilities
+import com.todoroo.andlib.utility.DateUtilities.now
 import com.todoroo.astrid.data.Task.Priority.Companion.HIGH
 import com.todoroo.astrid.data.Task.Priority.Companion.LOW
 import com.todoroo.astrid.data.Task.Priority.Companion.MEDIUM
@@ -23,6 +24,7 @@ fun com.todoroo.astrid.data.Task.applyRemote(
 ): com.todoroo.astrid.data.Task {
     applyCompletedAt(remote, local)
     applyCreatedAt(remote, local)
+    applyModified(remote, local)
     applyTitle(remote, local)
     applyDescription(remote, local)
     applyPriority(remote, local)
@@ -61,7 +63,16 @@ private fun com.todoroo.astrid.data.Task.applyCreatedAt(remote: Task, local: Tas
     val localCreated = local?.createdAt?.let { newDateTime(it, UTC) }?.toLocal()?.millis
     if (localCreated == null || localCreated == creationDate) {
         remote.createdAt?.let {
-            creationDate = newDateTime(it, UTC).toLocal().millis
+            creationDate = newDateTime(it, UTC).toLocal().millis.coerceAtMost(now())
+        }
+    }
+}
+
+private fun com.todoroo.astrid.data.Task.applyModified(remote: Task, local: Task?) {
+    val localModified = local?.lastModified?.let { newDateTime(it, UTC) }?.toLocal()?.millis
+    if (localModified == null || localModified == modificationDate) {
+        remote.lastModified?.let {
+            modificationDate = newDateTime(it, UTC).toLocal().millis.coerceAtMost(now())
         }
     }
 }

commit b477623524de02c283f0b78800ebadd12d1125e1

Update timestamp on edits
Fix bugs introduced by 775289b05

app/src/main/java/com/todoroo/astrid/adapter/TaskAdapter.kt
@@ -443,6 +443,6 @@ open class TaskAdapter(
             task.parent = newParent
         }
         caldavDao.update(caldavTask.id, caldavTask.remoteParent)
-        taskDao.save(task.task, null)
+        taskDao.save(task.task)
     }
 }

app/src/main/java/com/todoroo/astrid/service/TaskCreator.kt
@@ -114,7 +114,7 @@ class TaskCreator @Inject constructor(
                 locationDao.insert(Geofence(place.uid, preferences))
             }
         }
-        taskDao.save(task, null)
+        taskDao.save(task)
         alarmDao.insert(task.getDefaultAlarms())
         return task
     }

app/src/main/java/com/todoroo/astrid/service/TaskDuplicator.kt
@@ -99,7 +99,7 @@ class TaskDuplicator @Inject constructor(
             alarmDao.insert(alarms.map { Alarm(clone.id, it.time, it.type) })
         }
         gcalHelper.createTaskEventIfEnabled(clone)
-        taskDao.save(clone, null) // TODO: delete me
+        taskDao.save(clone) // TODO: delete me
         taskAttachmentDao
             .getAttachmentsForTask(originalId)
             .map {

app/src/main/java/com/todoroo/astrid/timers/TimerPlugin.kt
@@ -55,7 +55,7 @@ class TimerPlugin @Inject constructor(
                 task.elapsedSeconds = task.elapsedSeconds + newElapsed
             }
         }
-        taskDao.update(task)
+        taskDao.update(task, taskDao.fetch(task.id))
         updateNotifications()
     }
 

app/src/main/java/org/tasks/data/TaskDao.kt
@@ -188,7 +188,7 @@ FROM recursive_tasks
     @Insert
     abstract suspend fun insert(task: Task): Long
 
-    suspend fun update(task: Task, original: Task? = null): Task =
+    suspend fun update(task: Task, original: Task?): Task =
         task
             .copy(
                 modificationDate = when {

app/src/main/java/org/tasks/ui/TaskEditViewModel.kt
@@ -300,7 +300,7 @@ class TaskEditViewModel @Inject constructor(
             selectedAlarms.value = selectedAlarms.value.filterNot { a -> a.type == TYPE_REL_END }
         }
 
-        taskDao.save(task, null)
+        taskDao.save(task)
 
         if (isNew || originalList != selectedList.value) {
             task.parent = 0

app/src/androidTest/java/org/tasks/data/CaldavDaoShiftTests.kt
@@ -112,7 +112,7 @@ class CaldavDaoShiftTests : InjectingTestCase() {
     fun ignoreDeletedTasksWhenShiftingDown() = runBlocking {
         val created = DateTime(2020, 5, 17, 9, 53, 17)
         addTask(with(CREATED, created))
-        taskDao.update(taskDao.fetch(tasks[0].id).apply { this?.deletionDate = now() }!!)
+        taskDao.update(taskDao.fetch(tasks[0].id).apply { this?.deletionDate = now() }!!, null)
 
         caldavDao.shiftDown("calendar", 0, created.toAppleEpoch())
 