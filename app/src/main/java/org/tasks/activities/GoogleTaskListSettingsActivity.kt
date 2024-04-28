a0e6a3416d8221ecbfa95e44fcd813a4ecca32d0
hady <akhapugin@gmail.com>
2024-04-28 20:43:43 +0300

    Squashed commit for refactoring project to move BaseListSettingsActivity to Compose

--- app/src/main/java/org/tasks/activities/GoogleTaskListSettingsActivity.kt
+++ app/src/main/java/org/tasks/activities/GoogleTaskListSettingsActivity.kt
@@ -1,15 +1,13 @@
 package org.tasks.activities
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.view.inputmethod.InputMethodManager
-import android.widget.ProgressBar
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.material.SnackbarHostState
 import androidx.lifecycle.lifecycleScope
-import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.composethemeadapter.MdcTheme
 import com.google.api.services.tasks.model.TaskList
 import com.todoroo.astrid.activity.MainActivity
 import com.todoroo.astrid.activity.TaskListFragment
@@ -22,6 +20,11 @@ import kotlinx.coroutines.withContext
 import org.tasks.LocalBroadcastManager
 import org.tasks.R
 import org.tasks.Strings.isNullOrEmpty
+import org.tasks.compose.ListSettings.ListSettingsSnackBar
+import org.tasks.data.CaldavAccount
+import org.tasks.data.CaldavCalendar
+import org.tasks.data.GoogleTaskListDao
+import org.tasks.themes.CustomIcons
 import org.tasks.data.dao.GoogleTaskListDao
 import org.tasks.data.entity.CaldavAccount
 import org.tasks.data.entity.CaldavCalendar
@@ -39,15 +42,14 @@ class GoogleTaskListSettingsActivity : BaseListSettingsActivity() {
     @Inject lateinit var taskDeleter: TaskDeleter
     @Inject lateinit var localBroadcastManager: LocalBroadcastManager
 
-    private lateinit var name: TextInputEditText
-    private lateinit var progressView: ProgressBar
-
     private var isNewList = false
     private lateinit var gtasksList: CaldavCalendar
     private val createListViewModel: CreateListViewModel by viewModels()
     private val renameListViewModel: RenameListViewModel by viewModels()
     private val deleteListViewModel: DeleteListViewModel by viewModels()
     override val defaultIcon = TasksIcons.LIST
+
+    val snackbar = SnackbarHostState()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         gtasksList = intent.getParcelableExtra(EXTRA_STORE_DATA)
@@ -61,21 +63,28 @@ class GoogleTaskListSettingsActivity : BaseListSettingsActivity() {
             selectedColor = gtasksList.color
             selectedIcon.update { gtasksList.icon }
         }
-        if (isNewList) {
-            name.requestFocus()
-            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.showSoftInput(name, InputMethodManager.SHOW_IMPLICIT)
-        } else {
-            name.setText(gtasksList.name)
-        }
+
+        if (!isNewList) textState.value = gtasksList.name!!
+
         if (createListViewModel.inProgress
                 || renameListViewModel.inProgress
                 || deleteListViewModel.inProgress) {
             showProgressIndicator()
         }
+
         createListViewModel.observe(this, this::onListCreated, this::requestFailed)
         renameListViewModel.observe(this, this::onListRenamed, this::requestFailed)
         deleteListViewModel.observe(this, this::onListDeleted, this::requestFailed)
+
+        setContent {
+            MdcTheme {
+                baseSettingsContent()
+
+                ListSettingsSnackBar(state = snackbar)
+
+            }
+        }
+
         updateTheme()
     }
 
@@ -86,14 +95,14 @@ class GoogleTaskListSettingsActivity : BaseListSettingsActivity() {
         get() = if (isNew) getString(R.string.new_list) else gtasksList.name!!
 
     private fun showProgressIndicator() {
-        progressView.visibility = View.VISIBLE
+        showProgress.value = true
     }
 
     private fun hideProgressIndicator() {
-        progressView.visibility = View.GONE
+        showProgress.value = false
     }
 
-    private fun requestInProgress() = progressView.visibility == View.VISIBLE
+    private fun requestInProgress() = showProgress.value
 
     override suspend fun save() {
         if (requestInProgress()) {
@@ -101,7 +110,7 @@ class GoogleTaskListSettingsActivity : BaseListSettingsActivity() {
         }
         val newName = newName
         if (isNullOrEmpty(newName)) {
-            toast(R.string.name_cannot_be_empty)
+            errorState.value = getString(R.string.name_cannot_be_empty)
             return
         }
         when {
@@ -133,14 +142,7 @@ class GoogleTaskListSettingsActivity : BaseListSettingsActivity() {
     }
 
     override fun finish() {
-        hideKeyboard(name)
         super.finish()
-    }
-
-    override fun bind() = ActivityGoogleTaskListSettingsBinding.inflate(layoutInflater).let {
-        name = it.name
-        progressView = it.progressBar.progressBar
-        it.root
     }
 
     override fun promptDelete() {
@@ -161,7 +163,7 @@ class GoogleTaskListSettingsActivity : BaseListSettingsActivity() {
     }
 
     private val newName: String
-        get() = name.text.toString().trim { it <= ' ' }
+        get() = textState.value.trim { it <= ' ' }
 
     override fun hasChanges(): Boolean =
         if (isNewList) {
@@ -219,7 +221,8 @@ class GoogleTaskListSettingsActivity : BaseListSettingsActivity() {
     private fun requestFailed(error: Throwable) {
         Timber.e(error)
         hideProgressIndicator()
-        toast(R.string.gtasks_GLA_errorIOAuth)
+        lifecycleScope.launch { snackbar.showSnackbar(getString(R.string.gtasks_GLA_errorIOAuth)) }
+        //toast(R.string.gtasks_GLA_errorIOAuth)
         return
     }
 
---
+++ app/src/main/java/org/tasks/compose/FilterCondition.kt
@@ -0,0 +1,447 @@
+package org.tasks.compose
+
+/**
+ *  Composables for FilterSettingActivity
+ **/
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.outlined.Abc
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableIntState
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.core.os.ConfigurationCompat
+import com.todoroo.astrid.core.CriterionInstance
+import org.tasks.R
+import org.tasks.compose.ListSettings.ListSettingRow
+import org.tasks.compose.SwipeOut.SwipeOut
+import org.tasks.extensions.formatNumber
+import java.util.Locale
+
+@Composable
+@Preview (showBackground = true)
+fun ToggleGroupPreview ()
+{
+    FilterCondition.ToggleGroup (
+        items = listOf("AND","OR","NOT")
+    )
+}
+
+object FilterCondition {
+    @OptIn(ExperimentalFoundationApi::class)
+    @Composable
+    fun FilterCondition(
+        items: SnapshotStateList<CriterionInstance>,
+        onDelete: (Int) -> Unit,
+        doSwap: (Int, Int) -> Unit,
+        onClick: (String) -> Unit
+    ) {
+
+        val getIcon: (CriterionInstance) -> Int = { criterion ->
+            when (criterion.type) {
+                CriterionInstance.TYPE_ADD -> R.drawable.ic_call_split_24px
+                CriterionInstance.TYPE_SUBTRACT -> R.drawable.ic_outline_not_interested_24px
+                CriterionInstance.TYPE_INTERSECT -> R.drawable.ic_outline_add_24px
+                else -> {
+                    0
+                }  /* assert */
+            }
+        }
+        val listState = rememberLazyListState()
+        val dragDropState = rememberDragDropState(
+            lazyListState = listState,
+            confirmDrag = { index -> index != 0 }
+        ) { fromIndex, toIndex ->
+            if (fromIndex != 0 && toIndex != 0) doSwap(fromIndex, toIndex)
+        }
+
+        Row {
+            Text(
+                text = stringResource(id = R.string.custom_filter_criteria),
+                color = MaterialTheme.colors.secondary,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(Constants.KEYLINE_FIRST)
+            )
+        }
+        Row {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .doDrag(dragDropState),
+                userScrollEnabled = true,
+                state = listState
+            ) {
+                itemsIndexed(
+                    items = items,
+                    key = { _, item -> item.id }
+                ) { index, criterion ->
+                    if (index == 0) {
+                        FilterConditionRow(criterion, false, getIcon, onClick)
+                    } else {
+                        DraggableItem(
+                            dragDropState = dragDropState, index = index
+                        ) { dragging ->
+                            SwipeOut(
+                                decoration = { SwipeOutDecoration() },
+                                onSwipe = { index -> onDelete(index) },
+                                index = index
+                            ) {
+                                FilterConditionRow(criterion, dragging, getIcon, onClick)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    } /* FilterCondition */
+
+    @Composable
+    private fun FilterConditionRow(
+        criterion: CriterionInstance,
+        dragging: Boolean,
+        getIcon: (CriterionInstance) -> Int,
+        onClick: (String) -> Unit
+    ) {
+        Divider(
+            color = when (criterion.type) {
+                CriterionInstance.TYPE_ADD -> Color.Gray
+                else -> Color.Transparent
+            }
+        )
+        val modifier =
+            if (dragging) Modifier.background(Color.LightGray)
+            else Modifier
+        ListSettingRow(
+            modifier = modifier.clickable { onClick(criterion.id) },
+            left = {
+                Box(
+                    modifier = Modifier.size(56.dp),
+                    contentAlignment = Alignment.Center
+                )
+                {
+                    if (criterion.type != CriterionInstance.TYPE_UNIVERSE) {
+                        Icon(
+                            modifier = Modifier.padding(Constants.KEYLINE_FIRST),
+                            painter = painterResource(id = getIcon(criterion)),
+                            contentDescription = null
+                        )
+                    }
+                }
+            },
+            center = {
+                Text(
+                    text = criterion.titleFromCriterion,
+                    fontSize = 18.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier
+                        .weight(0.8f)
+                        .padding(start = 16.dp, top = 16.dp, bottom = 16.dp)
+                )
+            },
+            right = {
+                val context = LocalContext.current
+                val locale = remember {
+                    ConfigurationCompat
+                        .getLocales(context.resources.configuration)
+                        .get(0)
+                        ?: Locale.getDefault()
+                }
+                Text(
+                    text = locale.formatNumber(criterion.max),
+                    modifier = Modifier.padding(end = Constants.KEYLINE_FIRST),
+                    color = Color.Gray,
+                    fontSize = 14.sp,
+                    textAlign = TextAlign.End
+                )
+            }
+        )
+    }
+
+    @Composable
+    private fun SwipeOutDecoration() {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.secondary)
+        ) {
+
+            @Composable
+            fun deleteIcon() {
+                Icon(
+                    modifier = Modifier.padding(horizontal = 8.dp, vertical = 8.dp),
+                    imageVector = Icons.Filled.Delete,
+                    contentDescription = "Delete",
+                    tint = Color.White
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .height(56.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.Bottom
+            ) {
+                deleteIcon()
+                deleteIcon()
+            }
+        }
+    } /* end SwipeOutDecoration */
+
+    @Composable
+    fun NewCriterionFAB(
+        isExtended: MutableState<Boolean>,
+        onClick: () -> Unit
+    ) {
+
+        Box( // lays out over main content as a space to layout FAB
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.BottomEnd
+        ) {
+            FloatingActionButton(
+                onClick = onClick,
+                modifier = Modifier.padding(16.dp),
+                shape = RoundedCornerShape(50),
+                backgroundColor = MaterialTheme.colors.secondary,
+                contentColor = Color.White,
+            ) {
+                val extended = isExtended.value
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = "New Criteria",
+                        modifier = Modifier.padding(
+                            start = if (extended) 16.dp else 0.dp
+                        )
+                    )
+                    if (extended)
+                        Text(
+                            text = LocalContext.current.getString(R.string.CFA_button_add),
+                            modifier = Modifier.padding(end = 16.dp)
+                        )
+                }
+            } /* end FloatingActionButton */
+        }
+    } /* end NewCriterionFAB */
+
+    @Composable
+    fun SelectCriterionType(
+        title: String,
+        selected: Int,
+        types: List<String>,
+        onCancel: () -> Unit,
+        help: () -> Unit = {},
+        onSelected: (Int) -> Unit
+    ) {
+        val selected = remember { mutableIntStateOf(selected) }
+
+        Dialog(onDismissRequest = onCancel)
+        {
+            Card(
+                backgroundColor = MaterialTheme.colors.background
+            ) {
+                Column(modifier = Modifier.padding(horizontal = Constants.KEYLINE_FIRST)) {
+                    Text(
+                        text = title,
+                        color = MaterialTheme.colors.onSurface,
+                        style = MaterialTheme.typography.h6,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(48.dp)
+                            .padding(top = 16.dp)
+                    )
+                    ToggleGroup(items = types, selected = selected)
+                    Row(
+                        modifier = Modifier.height(48.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Box(contentAlignment = Alignment.CenterStart) {
+                            Constants.TextButton(text = R.string.help, onClick = help)
+                        }
+                        Box(
+                            contentAlignment = Alignment.CenterEnd,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Row {
+                                Constants.TextButton(text = R.string.cancel, onClick = onCancel)
+                                Constants.TextButton(text = R.string.ok) { onSelected(selected.intValue) }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    } /* end SelectCriterionType */
+
+    @Composable
+    fun ToggleGroup(
+        items: List<String>,
+        selected: MutableIntState = remember { mutableIntStateOf(0) }
+    ) {
+        assert(selected.intValue in items.indices)
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Row {
+                for (index in items.indices) {
+                    val highlight = (index == selected.intValue)
+                    val color =
+                        if (highlight) MaterialTheme.colors.secondary.copy(alpha = 0.5f)
+                        else MaterialTheme.colors.onBackground.copy(alpha = 0.5f)
+                    OutlinedButton(
+                        onClick = { selected.intValue = index },
+                        border = BorderStroke(1.dp, SolidColor(color.copy(alpha = 0.5f))),
+                        colors = ButtonDefaults.outlinedButtonColors(
+                            backgroundColor = color.copy(alpha = 0.2f),
+                            contentColor = MaterialTheme.colors.onBackground),
+                        shape = RoundedCornerShape(Constants.HALF_KEYLINE)
+                    ) {
+                        Text(items[index])
+                    }
+                    if (index<items.size-1) Spacer(modifier = Modifier.size(2.dp))
+                }
+            }
+        }
+    } /* end ToggleGroup */
+
+
+    @Composable
+    fun SelectFromList(
+        names: List<String>,
+        title: String? = null,
+        onCancel: () -> Unit,
+        onSelected: (Int) -> Unit
+    ) {
+        Dialog(onDismissRequest = onCancel) {
+            Card(backgroundColor = MaterialTheme.colors.background) {
+                Column(
+                    modifier = Modifier
+                        .padding(horizontal = Constants.KEYLINE_FIRST)
+                        .padding(bottom = Constants.KEYLINE_FIRST)
+                ) {
+                    title?.let { title ->
+                        Text(
+                            text = title,
+                            color = MaterialTheme.colors.onSurface,
+                            style = MaterialTheme.typography.h6,
+                            fontWeight = FontWeight.Medium,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(48.dp)
+                                .padding(top = Constants.KEYLINE_FIRST)
+                        )
+                    }
+                    names.forEachIndexed { index, name ->
+                        Text(
+                            text = name,
+                            color = MaterialTheme.colors.onSurface,
+                            style = MaterialTheme.typography.body1,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(48.dp)
+                                .padding(top = Constants.KEYLINE_FIRST)
+                                .clickable { onSelected(index) }
+                        )
+                    }
+                }
+            }
+        }
+    } /* end SelectFromList */
+
+
+    @Composable
+    fun InputTextOption(
+        title: String,
+        onCancel: () -> Unit,
+        onDone: (String) -> Unit
+    ) {
+        val text = remember { mutableStateOf("") }
+        AlertDialog(
+            onDismissRequest = onCancel,
+            confirmButton = {
+                Constants.TextButton(
+                    text = R.string.ok,
+                    onClick = { onDone(text.value) })
+            },
+            dismissButton = { Constants.TextButton(text = R.string.cancel, onClick = onCancel) },
+            text = {
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Text(
+                        title,
+                        style = MaterialTheme.typography.h6,
+                    )
+                    Spacer(Modifier.height(Constants.KEYLINE_FIRST))
+                    OutlinedTextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = text.value,
+                        label = { Text(title) },
+                        onValueChange = { text.value = it },
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Outlined.Abc,
+                                contentDescription = null
+                            )
+                        },
+                        textStyle = MaterialTheme.typography.body1,
+                        colors = Constants.textFieldColors(),
+                    )
+                }
+
+            }
+        )
+    } /* end InputTextOption */
+}
