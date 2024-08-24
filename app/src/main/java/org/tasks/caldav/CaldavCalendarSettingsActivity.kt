b38e8fa015845c11a9de8a88550dab6151613bf2
hady <akhapugin@gmail.com>
2024-08-24 11:44:35 +0300

    everything moved to Material3

--- app/src/main/java/org/tasks/caldav/CaldavCalendarSettingsActivity.kt
+++ app/src/main/java/org/tasks/caldav/CaldavCalendarSettingsActivity.kt
@@ -6,13 +6,12 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.AlertDialog
-import androidx.compose.material.Text
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
