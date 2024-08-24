af8721d44ca01fae965113dcedfb04bffcdb3dff
hady <akhapugin@gmail.com>
2024-08-24 10:20:08 +0300

    FilterSettingActivity moved to Material3

--- app/src/main/java/org/tasks/activities/FilterSettingsActivity.kt
+++ app/src/main/java/org/tasks/activities/FilterSettingsActivity.kt
@@ -6,8 +6,8 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Help
 import androidx.compose.runtime.Composable
