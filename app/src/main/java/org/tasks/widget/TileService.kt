9baa95ac81c81f75cd6c20314735a0680560b429
Alex Baker <alex@tasks.org>
2024-09-28 02:05:00 -0500

    Target Android 14

--- app/src/main/java/org/tasks/widget/TileService.kt
+++ app/src/main/java/org/tasks/widget/TileService.kt
@@ -1,12 +1,24 @@
 package org.tasks.widget
 
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.service.quicksettings.TileService
+import androidx.core.service.quicksettings.PendingIntentActivityWrapper
+import androidx.core.service.quicksettings.TileServiceCompat
 import org.tasks.intents.TaskIntents
 
 class TileService : TileService() {
     override fun onClick() {
         val newTaskIntent = TaskIntents.getNewTaskIntent(this, null, "tile")
                 .addFlags(TaskIntents.FLAGS)
-        startActivityAndCollapse(newTaskIntent)
+        TileServiceCompat.startActivityAndCollapse(
+            this,
+            PendingIntentActivityWrapper(
+                this,
+                0,
+                newTaskIntent,
+                FLAG_UPDATE_CURRENT,
+                false
+            )
+        )
     }
 }
