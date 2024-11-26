2501e588fe28de73c4436ecffcbdd6dbbf008f6f
Alex Baker <alex@tasks.org>
2024-11-26 01:24:05 -0600

    Fix comment delete button color

--- app/src/main/java/org/tasks/compose/DeleteButton.kt
+++ app/src/main/java/org/tasks/compose/DeleteButton.kt
@@ -1,13 +1,11 @@
 package org.tasks.compose
 
-import androidx.compose.material.ContentAlpha
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.stringResource
 import org.tasks.R
 
@@ -16,8 +14,8 @@ fun DeleteButton(onClick: () -> Unit) {
     IconButton(onClick = onClick) {
         Icon(
             imageVector = Icons.Outlined.Delete,
-            modifier = Modifier.alpha(ContentAlpha.medium),
-            contentDescription = stringResource(id = R.string.delete)
+            contentDescription = stringResource(id = R.string.delete),
+            tint = MaterialTheme.colorScheme.onSurface,
         )
     }
 }
