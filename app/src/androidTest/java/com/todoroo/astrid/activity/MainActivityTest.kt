fe7a55f909ef3057aa8b59c5ed13567199b044f5
Alex Baker <alex@tasks.org>
2025-03-25 23:49:18 -0500

    Fix tests

--- app/src/androidTest/java/com/todoroo/astrid/activity/MainActivityTest.kt
+++ app/src/androidTest/java/com/todoroo/astrid/activity/MainActivityTest.kt
@@ -4,11 +4,11 @@ import android.content.Intent
 import android.content.Intent.FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY
 import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.todoroo.astrid.activity.MainActivity.Companion.isFromHistory
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.tasks.extensions.isFromHistory
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
