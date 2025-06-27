b0f1b54403e76f26077c6efd8f88988a03ae6cc8
Samuel Born <samuelborn@outlook.de>
2025-06-27 10:53:21 +0200

    Add custom widget theme with color picker
    This commit introduces a new "Custom" theme option for the widget,
    allowing users to select a custom background color.
    
    Changes include:
    - Added "Custom" theme to widget_themes array in arrays.xml.
    - Introduced new string resources for custom theme color in strings.xml.
    - Modified preferences_widget.xml to include a color picker preference
    for the custom theme.
    - Updated WidgetSettings.kt to manage the visibility and functionality
    of the custom color picker based on theme selection.
    - Extended WidgetPreferences.kt to store and retrieve the custom theme
    color, and to apply it to the widget's background.
    - Implemented a isLight utility function in Theme.kt to determine text
    color based on the custom background color.

--- app/src/main/java/org/tasks/themes/Theme.kt
+++ app/src/main/java/org/tasks/themes/Theme.kt
@@ -1,6 +1,7 @@
 package org.tasks.themes
 
 import android.app.Activity
+import androidx.core.graphics.ColorUtils
 import javax.inject.Inject
 
 class Theme @Inject constructor(
@@ -14,5 +15,11 @@ class Theme @Inject constructor(
 
     fun applyTheme(activity: Activity) {
         themeBase.set(activity)
+    }
+
+    companion object {
+        fun isLight(color: Int): Boolean {
+            return ColorUtils.calculateLuminance(color) > 0.5
+        }
     }
 }
