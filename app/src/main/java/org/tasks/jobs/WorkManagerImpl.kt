7d2ebf9cdf043a87d6d445da9db36d87f32eb117
Alex Baker <alex@tasks.org>
2025-08-02 09:53:37 -0500

    Use ExistingPeriodicWorkPolicy.UPDATE

--- app/src/main/java/org/tasks/jobs/WorkManagerImpl.kt
+++ app/src/main/java/org/tasks/jobs/WorkManagerImpl.kt
@@ -136,7 +136,7 @@ class WorkManagerImpl(
                     .setConstraints(networkConstraints)
                 workManager.enqueueUniquePeriodicWork(
                     TAG_BACKGROUND_SYNC,
-                    ExistingPeriodicWorkPolicy.KEEP,
+                    ExistingPeriodicWorkPolicy.UPDATE,
                     builder.build()
                 )
             } else {
@@ -183,7 +183,7 @@ class WorkManagerImpl(
         throttle.run {
             workManager.enqueueUniquePeriodicWork(
                     TAG_REMOTE_CONFIG,
-                    ExistingPeriodicWorkPolicy.KEEP,
+                    ExistingPeriodicWorkPolicy.UPDATE,
                     PeriodicWorkRequest.Builder(
                             RemoteConfigWork::class.java, REMOTE_CONFIG_INTERVAL_HOURS, TimeUnit.HOURS)
                             .setConstraints(networkConstraints)
