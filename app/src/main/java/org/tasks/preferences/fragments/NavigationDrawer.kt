6318d8e703f72f2f3b11f450d1295aa4b4282e4b
Alex Baker <alex@tasks.org>
2026-02-11 19:06:35 -0600

    Add right arrow to customize drawer preference

--- app/src/main/java/org/tasks/preferences/fragments/NavigationDrawer.kt
+++ app/src/main/java/org/tasks/preferences/fragments/NavigationDrawer.kt
@@ -1,14 +1,25 @@
 package org.tasks.preferences.fragments
 
 import android.os.Bundle
+import androidx.appcompat.content.res.AppCompatResources
 import dagger.hilt.android.AndroidEntryPoint
 import org.tasks.R
 import org.tasks.injection.InjectingPreferenceFragment
+import org.tasks.preferences.IconPreference
 
 @AndroidEntryPoint
 class NavigationDrawer : InjectingPreferenceFragment() {
 
     override fun getPreferenceXml() = R.xml.preferences_navigation_drawer
 
-    override suspend fun setupPreferences(savedInstanceState: Bundle?) {}
+    override suspend fun setupPreferences(savedInstanceState: Bundle?) {
+        (findPreference(R.string.customize_drawer) as IconPreference).apply {
+            drawable = AppCompatResources.getDrawable(
+                requireContext(),
+                R.drawable.ic_keyboard_arrow_right_24px
+            )?.mutate()
+            tint = context.getColor(R.color.icon_tint_with_alpha)
+            iconVisible = true
+        }
+    }
 }
