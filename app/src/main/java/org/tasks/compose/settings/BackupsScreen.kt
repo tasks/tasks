488621be483a33c1da5d7d983a38a48337faeb1e
Florian Gubler <florian.gubler@gmail.com>
2026-03-07 20:50:55 +0100

    Let the user show/hide the password

--- app/src/main/java/org/tasks/compose/settings/BackupsScreen.kt
+++ app/src/main/java/org/tasks/compose/settings/BackupsScreen.kt
@@ -16,17 +16,25 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.OpenInNew
 import androidx.compose.material.icons.outlined.ErrorOutline
+import androidx.compose.material.icons.outlined.Visibility
+import androidx.compose.material.icons.outlined.VisibilityOff
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.input.VisualTransformation
 import org.tasks.R
 
 @Composable
@@ -122,14 +130,26 @@ fun BackupsScreen(
                 )
             }
             if (backupsEncryptionEnabled) {
+                var passwordVisible by rememberSaveable { mutableStateOf(false) }
                 SettingsItemCard(position = CardPosition.Last) {
                     OutlinedTextField(
                         value = backupsEncryptionPassword,
                         onValueChange = onBackupsEncryptionPassword,
                         label = { Text(stringResource(R.string.password)) },
-                        visualTransformation = PasswordVisualTransformation(),
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                        visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                        keyboardOptions = KeyboardOptions(keyboardType = if (passwordVisible) KeyboardType.Text else KeyboardType.Password),
+                        trailingIcon = {
+                            val image = if (passwordVisible) Icons.Outlined.Visibility else Icons.Outlined.VisibilityOff
+
+                            IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                                Icon(
+                                    imageVector = image,
+                                    contentDescription = stringResource(R.string.toggle_password_visibility)
+                                )
+                            }
+                        },
                         modifier = Modifier
+                            .fillMaxWidth()
                             .padding(
                                 start = SettingsContentPadding * 2 + SettingsIconSize,
                                 end = SettingsContentPadding,
