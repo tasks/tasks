922043d2cf35257f0e918779b21ae5d8f87a5568
Jason Held <jasonsheld@gmail.com>
2026-05-01 13:30:25 -0400

    switch to using strings in about, and what's rendered correctly without escaping

--- composeApp/src/commonMain/kotlin/org/tasks/compose/settings/HelpAndFeedbackScreen.kt
+++ composeApp/src/commonMain/kotlin/org/tasks/compose/settings/HelpAndFeedbackScreen.kt
@@ -21,7 +21,19 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.stringResource
 import org.tasks.kmp.JvmBuildConfig
+import tasks.kmp.generated.resources.Res
+import tasks.kmp.generated.resources.contact_developer
+import tasks.kmp.generated.resources.documentation
+import tasks.kmp.generated.resources.gplv3_license
+import tasks.kmp.generated.resources.issue_tracker
+import tasks.kmp.generated.resources.legal
+import tasks.kmp.generated.resources.privacy_policy
+import tasks.kmp.generated.resources.source_code
+import tasks.kmp.generated.resources.support
+import tasks.kmp.generated.resources.version_string
+import tasks.kmp.generated.resources.whats_new
 
 @Composable
 fun HelpAndFeedbackScreen(
@@ -47,9 +59,9 @@ fun HelpAndFeedbackScreen(
         ) {
             SettingsItemCard(position = CardPosition.Only) {
                 PreferenceRow(
-                    title = "What's New",
+                    title = stringResource(Res.string.whats_new),
                     icon = Icons.Outlined.NewReleases,
-                    summary = "Version $versionName",
+                    summary = stringResource(Res.string.version_string, versionName),
                     onClick = onDocumentation,
                 )
             }
@@ -57,7 +69,7 @@ fun HelpAndFeedbackScreen(
 
         // Support section
         SectionHeader(
-            "Support",
+            stringResource(Res.string.support),
             modifier = Modifier.padding(horizontal = 16.dp),
         )
         Column(
@@ -66,30 +78,30 @@ fun HelpAndFeedbackScreen(
         ) {
             SettingsItemCard(position = CardPosition.First) {
                 PreferenceRow(
-                    title = "Documentation",
+                    title = stringResource(Res.string.documentation),
                     icon = Icons.Outlined.HelpOutline,
                     onClick = onDocumentation,
                 )
             }
             SettingsItemCard(position = CardPosition.Middle) {
                 PreferenceRow(
-                    title = "Issue Tracker",
+                    title = stringResource(Res.string.issue_tracker),
                     icon = Icons.Outlined.BugReport,
                     onClick = onIssueTracker,
                 )
             }
             SettingsItemCard(position = CardPosition.Middle) {
                 PreferenceRow(
-                    title = "Contact Developer",
+                    title = stringResource(Res.string.contact_developer),
                     icon = Icons.Outlined.Email,
                     onClick = onContactDeveloper,
                 )
             }
             SettingsItemCard(position = CardPosition.Last) {
                 PreferenceRow(
-                    title = "Source Code",
+                    title = stringResource(Res.string.source_code),
                     icon = Icons.Outlined.NewReleases,
-                    summary = "GPLv3 License",
+                    summary = stringResource(Res.string.gplv3_license),
                     onClick = onSourceCode,
                 )
             }
@@ -97,7 +109,7 @@ fun HelpAndFeedbackScreen(
 
         // Legal section
         SectionHeader(
-            "Legal",
+            stringResource(Res.string.legal),
             modifier = Modifier.padding(horizontal = 16.dp),
         )
         Column(
@@ -106,7 +118,7 @@ fun HelpAndFeedbackScreen(
         ) {
             SettingsItemCard(position = CardPosition.Only) {
                 PreferenceRow(
-                    title = "Privacy Policy",
+                    title = stringResource(Res.string.privacy_policy),
                     icon = Icons.Outlined.PermIdentity,
                     onClick = onPrivacyPolicy,
                 )
