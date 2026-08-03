af6f55ab2884cce867958cc69f2705e5bf33288a
Alex Baker <alex@tasks.org>
2026-08-03 15:06:38 -0500

    Disable swipe-to-snooze for location reminders
    Remove 'Snoozed reminder' description

--- data/src/commonMain/kotlin/org/tasks/data/dao/NotificationDao.kt
+++ data/src/commonMain/kotlin/org/tasks/data/dao/NotificationDao.kt
@@ -28,4 +28,7 @@ interface NotificationDao {
 
     @Query("SELECT EXISTS(SELECT 1 FROM notification WHERE task = :taskId)")
     suspend fun hasNotification(taskId: Long): Boolean
+
+    @Query("SELECT type FROM notification WHERE task = :taskId")
+    suspend fun getType(taskId: Long): Int?
 }
