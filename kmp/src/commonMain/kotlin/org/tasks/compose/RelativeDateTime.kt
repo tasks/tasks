1ad8faba9168c3d01fdebb1a9160c356457ab83c
Alex Baker <alex@tasks.org>
2026-08-05 11:11:00 -0500

    Update dateformatter on clock and locale changes

--- kmp/src/commonMain/kotlin/org/tasks/compose/RelativeDateTime.kt
+++ kmp/src/commonMain/kotlin/org/tasks/compose/RelativeDateTime.kt
@@ -1,10 +1,12 @@
 package org.tasks.compose
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.produceState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -15,6 +17,7 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.stateIn
 import org.tasks.kmp.org.tasks.time.DateFormatter
 import org.tasks.kmp.org.tasks.time.DateStyle
+import org.tasks.kmp.org.tasks.time.currentLocaleTag
 import org.tasks.time.DateTimeUtils2.currentTimeMillis
 import org.tasks.time.plusDays
 import org.tasks.time.startOfDay
@@ -39,9 +42,13 @@ private val todayFlow: StateFlow<Long> =
 
 @Composable
 fun rememberDateFormatter(is24Hour: Boolean): DateFormatter? {
-    val formatter by produceState(DateFormatter.cachedOrNull(is24Hour), is24Hour) {
-        if (value == null) {
-            value = DateFormatter.create(is24Hour)
+    val localeTag = currentLocaleTag()
+    var formatter by remember(is24Hour, localeTag) {
+        mutableStateOf(DateFormatter.cachedOrNull(is24Hour))
+    }
+    LaunchedEffect(is24Hour, localeTag) {
+        if (formatter == null) {
+            formatter = DateFormatter.create(is24Hour)
         }
     }
     return formatter
