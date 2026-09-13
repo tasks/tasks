3daf4f0649ef2c0503a3514288216f8f2c1f21ed
Alex Baker <alex@tasks.org>
2026-09-13 19:04:45 +0000

    Move App to commonMain

--- composeApp/src/iosMain/kotlin/org/tasks/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/tasks/MainViewController.kt
@@ -1,14 +1,24 @@
 package org.tasks
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.ComposeUIViewController
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import org.koin.compose.koinInject
 import org.koin.core.context.startKoin
 import org.koin.mp.KoinPlatform
+import org.tasks.auth.TasksServerEnvironment
 import org.tasks.di.coreModule
 import org.tasks.di.platformModule
 import org.tasks.preferences.AppPreferences
 import org.tasks.preferences.recordInstallIfNeeded
 import org.tasks.service.Upgrader
+import platform.Foundation.NSURL
+import platform.UIKit.UIApplication
 
 private var started = false
 
@@ -28,4 +38,20 @@ private fun ensureStarted() {
 
 fun MainViewController() = ComposeUIViewController {
     ensureStarted()
+    val serverEnv = koinInject<TasksServerEnvironment>()
+    val scope = rememberCoroutineScope()
+    var currentEnv by remember { mutableStateOf(serverEnv.currentEnvironment) }
+    App(
+        openUrl = { url ->
+            NSURL.URLWithString(url)?.let { UIApplication.sharedApplication.openURL(it) }
+        },
+        environments = serverEnv.environments,
+        currentEnvironment = currentEnv,
+        onSelectEnvironment = { env ->
+            scope.launch {
+                serverEnv.setEnvironment(env)
+                currentEnv = env
+            }
+        },
+    )
 }
