7a1459b942f923c30e49e7b737682dea250f2dc5
Alex Baker <alex@tasks.org>
2026-09-14 07:22:49 +0000

    Wire up Google Tasks sign in on desktop

--- composeApp/src/commonMain/kotlin/org/tasks/compose/settings/GoogleTasksAccountSettingsDetail.kt
+++ composeApp/src/commonMain/kotlin/org/tasks/compose/settings/GoogleTasksAccountSettingsDetail.kt
@@ -1,6 +1,15 @@
 package org.tasks.compose.settings
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import co.touchlab.kermit.Logger
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.launch
+import org.koin.compose.koinInject
+import org.tasks.auth.SignInHandler
+import org.tasks.compose.accounts.Platform
+import org.tasks.sync.SyncAdapters
+import org.tasks.sync.SyncSource
 import tasks.kmp.generated.resources.Res
 import tasks.kmp.generated.resources.gtasks_GPr_header
 
@@ -9,9 +18,24 @@ fun GoogleTasksAccountSettingsDetail(
     pane: GoogleTasksAccountSettingsPane,
     onNavigateBack: () -> Unit,
 ) {
+    val signInHandler = koinInject<SignInHandler>()
+    val syncAdapters = koinInject<SyncAdapters>()
+    val scope = rememberCoroutineScope()
     AccountSettingsDetail(
         account = pane.account,
         defaultName = Res.string.gtasks_GPr_header,
         onNavigateBack = onNavigateBack,
+        onSignIn = {
+            scope.launch {
+                try {
+                    signInHandler.signIn(Platform.GOOGLE_TASKS)
+                    syncAdapters.sync(SyncSource.USER_INITIATED)
+                } catch (e: CancellationException) {
+                    throw e
+                } catch (e: Exception) {
+                    Logger.e(e) { "Google Tasks re-authentication failed" }
+                }
+            }
+        },
     )
 }
